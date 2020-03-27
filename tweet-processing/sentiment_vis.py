330a2231523f729c2eaa972f3724d7f898f71aae
leriomaggio <valerio.maggio@gmail.com>
2020-03-27 18:35:07 +0000

    PEP8 reformatting, removed unused imports, minor fixings

--- tweet-processing/sentiment_vis.py
+++ tweet-processing/sentiment_vis.py
@@ -1,20 +1,20 @@
-import pandas as pd 
-import datetime
+import pandas as pd
 
-#Setting up to use with iPython
+# Setting up to use with iPython
 import matplotlib.pyplot as plt
-%matplotlib inline
+
+# %matplotlib inline
 
 ###################
-#Read in the tweets
+# Read in the tweets
 tweets = pd.read_pickle("./25032020_corona_tweets.pkl")
 
 # Subset to just Wales based 'places' 
 tweets = tweets[tweets['place.full_name'].str.contains('Wales', regex=False, na=False)]
 
-#Mapping Sentiment
+# Mapping Sentiment
 
-#get the hourly average of the composite vader tweets.
+# get the hourly average of the composite vader tweets.
 comphouravg = tweets['vader_comp'].resample('H').mean().to_frame()
 compdayavg = tweets['vader_comp'].resample('D').mean().to_frame()
 
@@ -30,83 +30,85 @@ compdayavg = tweets['vader_comp'].resample('D').mean().to_frame()
 # plt.grid(True)
 # plt.show()
 
-#Covid hashtags (look at taking these direct from spreadsheet)
+# Covid hashtags (look at taking these direct from spreadsheet)
 covid_keyws = ['COVID19',
-'coronavirus',
-'covid_19',
-'covid2019',
-'covid2019UK',
-'coronavirusuk',
-'covid_19uk',
-'coronauk',
-'coronavirusoutbreak',
-'covid-19uk',
-'coronaviruspandemic',
-'coronavirusupdates',
-'c19',
-'fightcovid19',
-'corona',
-'coronacrisis',
-'Italy',
-'WorkingFromHomeLife',
-'workingfromhome',
-'dogworkingfromhome',
-'workingfromhomewithkids',
-'workingfromhomewithdogs',
-'workfromhome',
-'homeschooling',
-'socialdistancing',
-'selfisolating',
-'selfisolation',
-'quarantine',
-'quarantinelife',
-'socialdistancingnow',
-'staythefhome',
-'stayathome',
-'flattenthecurve',
-'stayathomechallenge',
-'covidiot',
-'stayhomesavelives',
-'notgoingout',
-'lockdown',
-'socialdistance',
-'lockdownUKnow',
-'uklockdownnow',
-'shamblesstayathome',
-'tory',
-'covidiots',
-'covid19out',
-'shutthesites',
-'uklockdown',
-'coronaviruslockdownuk',
-'panicbuying',
-'stockpiling',
-'stockpilinguk',
-'Keepcalm',
-'selfcare',
-'calmness',
-'anxiety',
-'positivity',
-'innerpeace',
-'mentalhealth',
-'rest',
-'PublicHealthW',
-'BetsiCadwaladr',
-'NHS',
-'NHSwales',
-'Michael Gove',
-'quarantineonlineparty',
-'quarantinegames',
-'NHSheroes',
-'whenthisisallover',
-'NHScovidheroes']
+               'coronavirus',
+               'covid_19',
+               'covid2019',
+               'covid2019UK',
+               'coronavirusuk',
+               'covid_19uk',
+               'coronauk',
+               'coronavirusoutbreak',
+               'covid-19uk',
+               'coronaviruspandemic',
+               'coronavirusupdates',
+               'c19',
+               'fightcovid19',
+               'corona',
+               'coronacrisis',
+               'Italy',
+               'WorkingFromHomeLife',
+               'workingfromhome',
+               'dogworkingfromhome',
+               'workingfromhomewithkids',
+               'workingfromhomewithdogs',
+               'workfromhome',
+               'homeschooling',
+               'socialdistancing',
+               'selfisolating',
+               'selfisolation',
+               'quarantine',
+               'quarantinelife',
+               'socialdistancingnow',
+               'staythefhome',
+               'stayathome',
+               'flattenthecurve',
+               'stayathomechallenge',
+               'covidiot',
+               'stayhomesavelives',
+               'notgoingout',
+               'lockdown',
+               'socialdistance',
+               'lockdownUKnow',
+               'uklockdownnow',
+               'shamblesstayathome',
+               'tory',
+               'covidiots',
+               'covid19out',
+               'shutthesites',
+               'uklockdown',
+               'coronaviruslockdownuk',
+               'panicbuying',
+               'stockpiling',
+               'stockpilinguk',
+               'Keepcalm',
+               'selfcare',
+               'calmness',
+               'anxiety',
+               'positivity',
+               'innerpeace',
+               'mentalhealth',
+               'rest',
+               'PublicHealthW',
+               'BetsiCadwaladr',
+               'NHS',
+               'NHSwales',
+               'Michael Gove',
+               'quarantineonlineparty',
+               'quarantinegames',
+               'NHSheroes',
+               'whenthisisallover',
+               'NHScovidheroes']
 
 # Subset tweets based on keywords
 corona_tweets = tweets[tweets['text'].str.contains('|'.join(covid_keyws))]
 
 # Get the hourly average of the composite vader tweets.
-covidcomphouravg = corona_tweets['vader_comp'].resample('H').mean().to_frame().reset_index().rename(columns={"vader_comp": "covid_vader_comp"})
-covidcompdayavg = corona_tweets['vader_comp'].resample('D').mean().to_frame().reset_index().rename(columns={"vader_comp": "covid_vader_comp"})
+covidcomphouravg = corona_tweets['vader_comp'].resample('H').mean().to_frame().reset_index().rename(
+    columns={"vader_comp": "covid_vader_comp"})
+covidcompdayavg = corona_tweets['vader_comp'].resample('D').mean().to_frame().reset_index().rename(
+    columns={"vader_comp": "covid_vader_comp"})
 
 # # Hourly average of comp sentiment
 # plt.plot(covidcomphouravg)
@@ -134,18 +136,14 @@ plot_hourly_avg = pd.melt(hourly_avg, id_vars=['created_at'], value_vars=['covid
 
 p = ggplot(plot_hourly_avg) + geom_line(aes(x='created_at', y='value', color='variable'))
 p = p + labs(x="Date-Time", y="VADER Compound Sentiment", title="Hourly compound sentiment for all vs COVID tweets")
-p = p + theme(axis_text_x = element_text(angle=90, vjust=1))
+p = p + theme(axis_text_x=element_text(angle=90, vjust=1))
 
 # Reshape the daily dataframe to long format for plotting
 plot_daily_avg = pd.melt(daily_avg, id_vars=['created_at'], value_vars=['covid_vader_comp', 'vader_comp'])
 
 p2 = ggplot(plot_daily_avg) + geom_line(aes(x='created_at', y='value', color='variable'))
 p2 = p2 + labs(x="Date-Time", y="VADER Compound Sentiment", title="Daily compound sentiment for all vs COVID tweets")
-p2 = p2 + theme(axis_text_x = element_text(angle=90, vjust=1))
-
-
+p2 = p2 + theme(axis_text_x=element_text(angle=90, vjust=1))
 
 p.save('hourly.png', bbox_inches='tight')
 p2.save('daily.png', bbox_inches='tight')
-
-
--- tweet-processing/tweet_processing.py
+++ tweet-processing/tweet_processing.py
@@ -1,69 +1,69 @@
 import pandas as pd
-from datetime import datetime
 import re
-#For text cleaning
+# For text cleaning
 import demoji
-#NB for first time use we need to download the lastest emoji codes (since they are updated reguarly)
-#demoji.download_codes()
+# FIXME: NB for first time use we need to download the lastest emoji codes (since they are updated reguarly)
+# demoji.download_codes()
 import nltk
-import vader
 import spacy
+# Next we will use VADER to do some basic sentiment analysis (on the original 'text', not 'nlp_text')
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 
-#Read in export from the Virtual Box VM (~420k tweets)
+# Read in export from the Virtual Box VM (~420k tweets)
 tweets_vbox = pd.read_csv('vm_export.csv')
-#Read in export from the Azure VM (~50k tweets)
+# Read in export from the Azure VM (~50k tweets)
 tweets_azure = pd.read_csv('25032020.csv')
 
-#Concatenate the tweets
+# Concatenate the tweets
 tweets = pd.concat([tweets_vbox, tweets_azure])
 
-#Parse 'created at' to pandas datetime - requires 'from datetime import datetime'
+# Parse 'created at' to pandas datetime - requires 'from datetime import datetime'
 tweets['created_at'] = pd.to_datetime(tweets['created_at'])
 
-#Set the id as the ID of the row
+# Set the id as the ID of the row
 tweets.index = tweets['created_at']
 del tweets['created_at']
 
-#We also seem to have two tweet ID columns ('id' and 'id_str'), so get rid of one of those. 
+# We also seem to have two tweet ID columns ('id' and 'id_str'), so get rid of one of those.
 del tweets['id_str']
 
-#Now that we have date-time we can take only those tweets after 8th March, as this is when I actually have data for
-#Since data collection stopped between 19th Feb and 8th March
+# Now that we have date-time we can take only those tweets after 8th March, as this is when I actually have data for
+# Since data collection stopped between 19th Feb and 8th March
 df = tweets['3/8/2020':]
 
-#Before we go any further, lets tidy up the text columns... 
+
+# Before we go any further, lets tidy up the text columns...
 
 def tidy_text_cols(data):
-    
-    """ Uses values from the short ('text') and extended ('extended_tweet.full_text') columns to make a single 'text' column with the 
+    """ Uses values from the short ('text') and extended ('extended_tweet.full_text') columns to make a single 'text' column with the
     full version of every tweet. """
-    
-    #keep the data for where 'tweet_full' is not used
+
+    # keep the data for where 'tweet_full' is not used
     keep = pd.isnull(data['extended_tweet.full_text'])
 
-    #Where tweet_full is used, make tweet_full as the text
+    # Where tweet_full is used, make tweet_full as the text
     data_valid = data[~keep]
     data.loc[~keep, 'text'] = data_valid['extended_tweet.full_text']
 
     return data
 
+
 df = tidy_text_cols(df)
 
-#and then get rid of the now redundant column
+# and then get rid of the now redundant column
 del df['extended_tweet.full_text']
-
 
 ######
 # Now we want to clean the text up a little bit to get it ready for analysis. 
 
 # Save the emoji used in the text in a seperate column 'emoji' 
-df['emoji'] = df['text'].apply(lambda x : demoji.findall(x))
+df['emoji'] = df['text'].apply(lambda x: demoji.findall(x))
 
 # We still want to keep the original tweet text because things like VADER use punctuation and capitals for sentiment.
 # So, lets make a new column for text that we can tokenise, called 'nlp_text'
 
 # Remove special characters
-df['nlp_text'] = df['text'].apply(lambda x : re.sub("[^a-z\s]","",x) )
+df['nlp_text'] = df['text'].apply(lambda x: re.sub("[^a-z\s]", "", x))
 
 # Lower case all words
 df['nlp_text'] = df['nlp_text'].str.lower()
@@ -71,28 +71,25 @@ df['nlp_text'] = df['nlp_text'].str.lower()
 # Removing stopwords (using stopword list from NLTK) - NB this takes AGES. 
 nltk.download('stopwords')
 from nltk.corpus import stopwords
-stopwords = set(stopwords.words("english"))
-df['nlp_text'] = df['nlp_text'].apply(lambda x : " ".join(word for word in x.split() if word not in stopwords ))
 
+stopwords = set(stopwords.words("english"))
+df['nlp_text'] = df['nlp_text'].apply(lambda x: " ".join(word for word in x.split() if word not in stopwords))
 
 #########
 # Now the text is a bit cleaner we can create a spaCy object for each tweet, and apply some basic sentiment analysis. 
 
 # First create a spaCy object ('doc') for each tweet, and save to a new column so we can call it. 
 nlp = spacy.load("en_core_web_sm")
-df['spacy_doc'] = df['nlp_text'].apply(lambda x : nlp(x))
-
-# Next we will use VADER to do some basic sentiemnt analysis (on the original 'text', not 'nlp_text')
-from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+df['spacy_doc'] = df['nlp_text'].apply(lambda x: nlp(x))
 
 # Define the sentiment analyser object
 analyser = SentimentIntensityAnalyzer()
 
-#Apply sentiment analysis to the data frame (new col for each)
-df['vader_comp'] = df['text'].apply(lambda x : analyser.polarity_scores(x)['compound'])
-df['vader_pos'] = df['text'].apply(lambda x : analyser.polarity_scores(x)['pos'])
-df['vader_neg'] = df['text'].apply(lambda x : analyser.polarity_scores(x)['neg'])
-df['vader_neu'] = df['text'].apply(lambda x : analyser.polarity_scores(x)['neu'])
+# Apply sentiment analysis to the data frame (new col for each)
+df['vader_comp'] = df['text'].apply(lambda x: analyser.polarity_scores(x)['compound'])
+df['vader_pos'] = df['text'].apply(lambda x: analyser.polarity_scores(x)['pos'])
+df['vader_neg'] = df['text'].apply(lambda x: analyser.polarity_scores(x)['neg'])
+df['vader_neu'] = df['text'].apply(lambda x: analyser.polarity_scores(x)['neu'])
 
 ########
 # Now the dataframe is read in, cleaned and prepared for analysis we can pickle it out and use another script to analyse it. 
