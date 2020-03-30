9f8cc5211d0bf621c8736727f6ce5126924ef535
CMorenoStokoe <CMorenoStokoe@users.noreply.github.com>
2020-03-30 18:51:55 +0100

    demographics python utility

--- geoJSON/MutualAid Live group utility/scripts.js
+++ geoJSON/MutualAid Live group utility/scripts.js
@@ -3,32 +3,46 @@ var output=[]
 var xhttp = new XMLHttpRequest();
 xhttp.open("GET", "https://raw.githubusercontent.com/Covid-Mutual-Aid/search-by-postcode/master/data/groups.json", true);
 xhttp.onload = function() {
-                var ourData = JSON.parse(xhttp.responseText);
-                console.log("GOT data: ",ourData);
-                ourData.forEach(reformat);
-                console.log("Reformatted data: ",output);
-                document.getElementById("output-div").innerHTML=JSON.stringify(
-                    {
-                    "type": "FeatureCollection",
-                    "features": output
-                    })
-                }
+                
+    //Parse JSON
+    var ourData = JSON.parse(xhttp.responseText);
+        console.log("GOT data: ", ourData);
+
+    //Reformat JSON to geoJSON
+    ourData.forEach(reformat);
+        console.log("Reformatted data: ", output);
+    
+    //Preview formatted geoJSON
+    //ourData.forEach(reformatToCSV);
+    document.getElementById("output-div").innerHTML=JSON.stringify(
+        {
+        "type": "FeatureCollection",
+        "features": output
+        })
+
+    /*document.getElementById("output-div").innerHTML=JSON.stringify({"names":names,"latitude":lats,"longitude":longs});
+    */
+}
 xhttp.send();
 
 function inWales(row){
     
+    //SOURCE DATA MIXES LNG AND LAT
     lat=row["location_coord"]["lat"]
     long=row["location_coord"]["lng"]
     id=row["id"]
     
-    if (lat <=90 && lat >=-90 && long <= 180 && long >=-180){
+    if (lat <=53.45 && lat >=51.35 && long >=-5.5 && long <=-2.65){
+            console.log("Welsh group identified: ", row)
         return(true)
     } else {
-        console.log("Group excluded: ")
+            console.log("Group excluded: ", row)
         return(false)}
 }
 
 function reformat(row){
+    
+    console.log("Formatting: ", row)
     
     if (inWales(row) == true){
         
@@ -36,14 +50,23 @@ function reformat(row){
               "type": "Feature",
               "geometry": {
                 "type": "Point",
-                "coordinates": [row["location_coord"]["lat"], row["location_coord"]["lng"]]
+                "coordinates": [row["location_coord"]["lng"], row["location_coord"]["lat"]]
               },
               "properties": {
                 "name": row["name"],
                 "location_name": row["location_name"],
                 "link": row["link_facebook"]
               }
-        })    
-    
+        }) 
     }
+}
+
+//OPTIONAL: Convert to CSV instead
+var names=[]
+var lats=[]
+var longs=[]
+function reformatToCSV(row){
+    names.push(row["name"])
+    lats.push(row["location_coord"]["lat"])
+    longs.push(row["location_coord"]["lng"])
 }
