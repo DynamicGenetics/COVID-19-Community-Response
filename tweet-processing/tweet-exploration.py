f39d7825a0e2b6325a162dae61cf43d1abdbebb6
Nina Di Cara <ninadicara@gmail.com>
2020-04-08 13:01:06 +0100

    Update to use tweet_functions

--- tweet-processing/tweet-exploration.py
+++ tweet-processing/tweet-exploration.py
@@ -1,43 +1,14 @@
 import pandas as pd
 import re
 import spacy
+from tweet_functions import *
 
 ###################
 # DATASET TIDYING #
 ###################
 
-# Read in export from the Virtual Box VM (~420k tweets)
-tweets = pd.read_csv('../../data/06042020.csv')
-
-# Filter out non-Wales tweets
-tweets = tweets[tweets['place.full_name'].str.contains('Wales', regex=False, na=False)]
-
-# Parse 'created at' to pandas datetime - requires 'from datetime import datetime'
-tweets['created_at'] = pd.to_datetime(tweets['created_at'])
-
-# Set the datetime of tweet creation as the dataframe index
-tweets.index = tweets['created_at']
-del tweets['created_at']
-
-# Before we go any further, lets tidy up the text columns...
-def tidy_text_cols(data):
-    """ Uses values from the short ('text') and extended ('extended_tweet.full_text') columns to make a single 'text' column with the
-    full version of every tweet. """
-
-    # keep the data for where 'tweet_full' is not used
-    keep = pd.isnull(data['extended_tweet.full_text'])
-
-    # Where tweet_full is used, make tweet_full as the text
-    data_valid = data[~keep]
-    data.loc[~keep, 'text'] = data_valid['extended_tweet.full_text']
-
-    return data
-
-
-tweets = tidy_text_cols(tweets)
-# and then get rid of the now redundant column
-del tweets['extended_tweet.full_text']
-
+# Read in
+tweets = read_and_tidy('../../data/06042020.csv')
 
 ###############
 # EXPLORATION #
