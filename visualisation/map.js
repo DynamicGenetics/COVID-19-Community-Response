8129428c7846f8a97d0f589f5d7535ac0d8feb74
leriomaggio <valerio.maggio@gmail.com>
2020-04-17 19:36:15 +0100

    MAP WORKING WITH TWEETS

--- visualisation/map.js
+++ visualisation/map.js
@@ -7,141 +7,154 @@ var map = new mapboxgl.Map({
     zoom: 7
 });
 
-// var layers = [
-//     {
-//         name: 'Local Authority boundaries',
-//         shownByDefault: true,
-//         layerSpec: {
-//             id: 'local-authority-boundaries',
-//             type: 'line',
-//             source: 'local-authority-boundaries',
-//             layout: {},
-//             paint: {
-//                 'line-color': '#088',
-//                 'line-opacity': 1.0
-//             }
-//         }
-//     },
-//     {
-//         name: 'Known groups',
-//         shownByDefault: true,
-//         layerSpec: {
-//             id: 'known-groups',
-//             type: 'circle',
-//             source: 'known-groups',
-//             paint: {
-//                 'circle-radius': {
-//                     base: 1.75,
-//                     stops: [[12, 2.7], [22, 180]]
-//                 },
-//                 'circle-color': '#7a0177'
-//             }
-//         }
-//     },
-//     {
-//         name: 'Index of Multiple Deprivation',
-//         shownByDefault: false,
-//         layerSpec: {
-//             id: 'deprivation',
-//             type: 'fill',
-//             source: 'demographics',
-//             paint: {
-//                 'fill-color': {
-//                     property: 'deprivation_30',
-//                     stops: [[5, '#ffcaff'],
-//                             [10.9, '#f0b7ed'],
-//                             [16.8, '#e1a4db'],
-//                             [22.7, '#d391ca'],
-//                             [28.6, '#c47fb9'],
-//                             [34.5, '#b56da8'],
-//                             [40.4, '#a75b98'],
-//                             [46.3, '#984887'],
-//                             [52.2, '#8a3678'],
-//                             [64, '#7b2268']]
-//                 },
-//                 'fill-opacity': 0.5
-//             },
-//             filter: ['==', '$type', 'Polygon']
-//         }
-//     },
-//     {
-//         name: 'Population density',
-//         shownByDefault: false,
-//         layerSpec: {
-//             id: 'pop_density',
-//             type: 'fill',
-//             source: 'demographics',
-//             paint: {
-//                 'fill-color': {
-//                     property: 'pop_density',
-//                     stops: [[25.6, '#ffff87'],
-//                             [281.56, '#fff77f'],
-//                             [537.52, '#ffef77'],
-//                             [793.48, '#ffe770'],
-//                             [1049.44, '#ffdf68'],
-//                             [1305.4, '#ffd760'],
-//                             [1561.36, '#ffcf58'],
-//                             [1817.32, '#ffc750'],
-//                             [2073.28, '#ffbe48'],
-//                             [2585.2, '#ffb63f']]
-//                 },
-//                 'fill-opacity': 0.5
-//             },
-//             filter: ['==', '$type', 'Polygon']
-//         }
-//     },
-//     {
-//         name: 'Elderly population',
-//         shownByDefault: false,
-//         layerSpec: {
-//             id: 'pop_elderly',
-//             type: 'fill',
-//             source: 'demographics',
-//             paint: {
-//                 'fill-color': {
-//                     property: 'pop_elderly',
-//                     stops: [[14.09451802, '#91e5ff'],
-//                             [15.429026416, '#84d3f3'],
-//                             [16.763534812, '#77c2e7'],
-//                             [18.098043208, '#6ab1da'],
-//                             [19.432551604, '#5da0ce'],
-//                             [20.76706, '#4f90c3'],
-//                             [22.101568396, '#417fb7'],
-//                             [23.436076792, '#3270ab'],
-//                             [24.770585188, '#1f609f'],
-//                             [27.43960198, '#005194']]
-//                 },
-//                 'fill-opacity': 0.5
-//             },
-//             filter: ['==', '$type', 'Polygon']
-//         }
-//     },
-//     {
-//         name: 'Welsh language',
-//         shownByDefault: false,
-//         layerSpec: {
-//             id: 'language',
-//             type: 'fill',
-//             source: 'demographics',
-//             paint: {
-//                 'fill-color': {
-//                     property: 'language',
-//                     stops: [[4.8, '#e689ff'],
-//                             [10.83, '#d87cf6'],
-//                             [16.86, '#ca70ed'],
-//                             [22.89, '#bd63e3'],
-//                             [28.92, '#af56da'],
-//                             [34.95, '#a249d1'],
-//                             [40.98, '#943cc8'],
-//                             [47.01, '#862fbf'],
-//                             [53.04, '#7820b6'],
-//                             [65.1, '#6a0dad']]
-//                 },
-//                 'fill-opacity': 0.5
-//             },
-//             filter: ['==', '$type', 'Polygon']
-//         }
-//     }
+var extra_layers = [
+    {
+        name: 'Tweets by LA Population',
+        shownByDefault: false,
+        layerSpec: {
+            id: 'tweet',
+            type: 'fill',
+            source: 'tweet',
+            paint: {
+                'fill-color': {
+                    property: 'tweets_per_pop',
+                    stops: [[0.1, '#F7FBFF'],
+                            [0.2, '#DEEBF7'],
+                            [0.3, '#C6DBEF'],
+                            [0.4, '#9ECAE1'],
+                            [0.5, '#6BAED6'],
+                            [0.6, '#4292C6'],
+                            [0.7, '#2171B5'],
+                            [0.8, '#984887'],
+                            [0.9, '#08519C'],
+                            [1, '#08306B']]
+                },
+                'fill-opacity': 0.46666666666666666
+            },
+            filter: ['==', '$type', 'Polygon']
+        }
+    },
+    ];
+    // {
+    //     name: 'Known groups',
+    //     shownByDefault: true,
+    //     layerSpec: {
+    //         id: 'known-groups',
+    //         type: 'circle',
+    //         source: 'known-groups',
+    //         paint: {
+    //             'circle-radius': {
+    //                 base: 1.75,
+    //                 stops: [[12, 2.7], [22, 180]]
+    //             },
+    //             'circle-color': '#7a0177'
+    //         }
+    //     }
+    // },
+    // {
+    //     name: 'Index of Multiple Deprivation',
+    //     shownByDefault: false,
+    //     layerSpec: {
+    //         id: 'deprivation',
+    //         type: 'fill',
+    //         source: 'demographics',
+    //         paint: {
+    //             'fill-color': {
+    //                 property: 'deprivation_30',
+    //                 stops: [[5, '#ffcaff'],
+    //                         [10.9, '#f0b7ed'],
+    //                         [16.8, '#e1a4db'],
+    //                         [22.7, '#d391ca'],
+    //                         [28.6, '#c47fb9'],
+    //                         [34.5, '#b56da8'],
+    //                         [40.4, '#a75b98'],
+    //                         [46.3, '#984887'],
+    //                         [52.2, '#8a3678'],
+    //                         [64, '#7b2268']]
+    //             },
+    //             'fill-opacity': 0.5
+    //         },
+    //         filter: ['==', '$type', 'Polygon']
+    //     }
+    // },
+    // {
+    //     name: 'Population density',
+    //     shownByDefault: false,
+    //     layerSpec: {
+    //         id: 'pop_density',
+    //         type: 'fill',
+    //         source: 'demographics',
+    //         paint: {
+    //             'fill-color': {
+    //                 property: 'pop_density',
+    //                 stops: [[25.6, '#ffff87'],
+    //                         [281.56, '#fff77f'],
+    //                         [537.52, '#ffef77'],
+    //                         [793.48, '#ffe770'],
+    //                         [1049.44, '#ffdf68'],
+    //                         [1305.4, '#ffd760'],
+    //                         [1561.36, '#ffcf58'],
+    //                         [1817.32, '#ffc750'],
+    //                         [2073.28, '#ffbe48'],
+    //                         [2585.2, '#ffb63f']]
+    //             },
+    //             'fill-opacity': 0.5
+    //         },
+    //         filter: ['==', '$type', 'Polygon']
+    //     }
+    // },
+    // {
+    //     name: 'Elderly population',
+    //     shownByDefault: false,
+    //     layerSpec: {
+    //         id: 'pop_elderly',
+    //         type: 'fill',
+    //         source: 'demographics',
+    //         paint: {
+    //             'fill-color': {
+    //                 property: 'pop_elderly',
+    //                 stops: [[14.09451802, '#91e5ff'],
+    //                         [15.429026416, '#84d3f3'],
+    //                         [16.763534812, '#77c2e7'],
+    //                         [18.098043208, '#6ab1da'],
+    //                         [19.432551604, '#5da0ce'],
+    //                         [20.76706, '#4f90c3'],
+    //                         [22.101568396, '#417fb7'],
+    //                         [23.436076792, '#3270ab'],
+    //                         [24.770585188, '#1f609f'],
+    //                         [27.43960198, '#005194']]
+    //             },
+    //             'fill-opacity': 0.5
+    //         },
+    //         filter: ['==', '$type', 'Polygon']
+    //     }
+    // },
+    // {
+    //     name: 'Welsh language',
+    //     shownByDefault: false,
+    //     layerSpec: {
+    //         id: 'language',
+    //         type: 'fill',
+    //         source: 'demographics',
+    //         paint: {
+    //             'fill-color': {
+    //                 property: 'language',
+    //                 stops: [[4.8, '#e689ff'],
+    //                         [10.83, '#d87cf6'],
+    //                         [16.86, '#ca70ed'],
+    //                         [22.89, '#bd63e3'],
+    //                         [28.92, '#af56da'],
+    //                         [34.95, '#a249d1'],
+    //                         [40.98, '#943cc8'],
+    //                         [47.01, '#862fbf'],
+    //                         [53.04, '#7820b6'],
+    //                         [65.1, '#6a0dad']]
+    //             },
+    //             'fill-opacity': 0.5
+    //         },
+    //         filter: ['==', '$type', 'Polygon']
+    //     }
+    // }
 // ];
 
 map.on('load', function () {
@@ -152,10 +165,21 @@ map.on('load', function () {
             'type': 'geojson',
             'data': layer.ref,
         });
-    };
+    }
+
+    map.addSource('tweet', {
+        'type': 'geojson',
+        'data': '../data/twitter_count.geojson'
+        });
 
     // Add Layers
     for (const layer of layers){
+        const visibility = layer.shownByDefault ? 'visible' : 'none';
+        map.addLayer(layer.layerSpec);
+        map.setLayoutProperty(layer.layerSpec.id, 'visibility', visibility);
+    }
+
+    for (const layer of extra_layers){
         const visibility = layer.shownByDefault ? 'visible' : 'none';
         map.addLayer(layer.layerSpec);
         map.setLayoutProperty(layer.layerSpec.id, 'visibility', visibility);
@@ -166,6 +190,32 @@ map.on('load', function () {
 var menu = document.getElementById('menu');
 
 for (const layer of layers) {
+    var id = layer.layerSpec.id;
+    var name = layer.name;
+    var checked = layer.shownByDefault;
+
+    var container = document.createElement('div');
+    var checkbox = document.createElement('input');
+    var label = document.createElement('label');
+
+    checkbox.type = 'checkbox';
+    checkbox.value = id;
+    checkbox.id = id;
+    checkbox.checked = checked;
+    checkbox.setAttribute('class', 'checkbox')
+
+    label.setAttribute('for', id);
+    label.textContent = name;
+
+    checkbox.addEventListener('change', checkboxChange);
+
+    container.appendChild(checkbox);
+    container.appendChild(label);
+
+    menu.appendChild(container);
+}
+
+for (const layer of extra_layers) {
     var id = layer.layerSpec.id;
     var name = layer.name;
     var checked = layer.shownByDefault;
