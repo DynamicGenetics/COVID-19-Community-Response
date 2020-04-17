1dd96a598d9f3e3333000eb4830595e8534344aa
leriomaggio <valerio.maggio@gmail.com>
2020-04-17 15:14:40 +0100

    added the Matching of Local Authorities to the pipeline & Moved DateTime Index creation at the end of the pipeline

--- tweet-processing/pipelines.py
+++ tweet-processing/pipelines.py
@@ -1,12 +1,25 @@
 # %%
 import pandas as pd
+import numpy as np
 import json
 from typing import Tuple, Callable, List
+from typing import Sequence, Tuple, Union
 from abc import ABC, abstractmethod
 from functools import partial
+from shapely.geometry import Polygon, Point
 
+# -------------------------------
+# Type Hints (Custom Definitions)
+# -------------------------------
+# Pipeline
 Pipe = Tuple[str, Callable[[pd.DataFrame], pd.DataFrame]]
 
+# Types for (Geo) functions
+Lat = float
+Long = float
+BoundingBox = Sequence[Tuple[Long, Lat]]
+MatchingLA = Union[Tuple[str, str, str], pd.DataFrame]
+# -------------------------------
 
 class Pipeline(ABC):
     """ABC implementing the general template of a
@@ -71,7 +84,10 @@ def _get_welsh_tweets(data: pd.DataFrame, col: str = "place.full_name"):
     """Filters dataframe to only include tweets from Wales,
     by querying on column with place.full_name ('col')."""
     # Filter out non-Wales tweets
-    return data[data[col].str.contains("Wales", regex=False, na=False)]
+    data = data[data[col].str.contains("Wales", regex=False, na=False)]
+    # filter out tweets referencing the whole Wales
+    # removed as we cannot match any of those to any LA later on.
+    return data[~data[col].str.contains('Wales, ')]
 
 
 def _create_datetime_index(data: pd.DataFrame) -> pd.DataFrame:
@@ -115,7 +131,7 @@ def _bbox_geojson(data: pd.DataFrame, col: str = 'place.bounding_box.coordinates
         bbox[0].append(bbox[0][0])
         return bbox
 
-    data["bbox_geojson"] = data[col].apply(lambda x: append_coords(x))
+    data['bbox_geojson'] = data[col].apply(lambda x: append_coords(x))
     return data
 
 
@@ -123,11 +139,141 @@ def _bbox_tuple(data: pd.DataFrame) -> pd.DataFrame:
     """Creates a new column in the data with BoundingBoxes encoded as
     tuples of floats (with no repetition). This column is meant
     to ease the processing to match closest local authority.
-
-    NOTE: So far, this Pipe step **must** come after _bbox_geojson
-    as it depends on it.
     """
-    data["bbox"] = data["bbox_geojson"].apply(lambda c: tuple(map(tuple, c[0][:-1])))
+    data['bbox'] = data['place.bounding_box.coordinates'].apply(lambda c: tuple(map(tuple, c[0][:-1])))
+    return data
+
+
+def convert_coordinates(data, col="geo.coordinates"):
+    """ Takes a Pandas dataframe with a column geo.coordinates (col)
+    and adds the lat and long to their own columns
+    for easy conversion to geojson. """
+
+    def convert_coords(geo_coords):
+        if not geo_coords:
+            return np.nan, np.nan
+        coords = tuple(map(float, geo_coords[1:-1].split(',')))
+        return coords
+
+    def set_geo_coords(geo_coords):
+        coords = convert_coords(geo_coords)
+        if (np.isnan(coords[0])):
+            return None
+        return coords[::-1]
+
+    # Substitute the nan with empty strings
+    data[col].fillna('', inplace=True)
+
+    data['lat'] = data[col].apply(lambda c: convert_coords(c)[0])
+    data['long'] = data[col].apply(lambda c: convert_coords(c)[1])
+    data['geo_coords'] = data[col].apply(lambda c: set_geo_coords(c))
+
+    return data
+
+
+# %%
+def match_local_authorities(bbox: BoundingBox, la_df: pd.DataFrame, return_all: bool = False)->MatchingLA:
+    """ Get the Intersection Over Union for the the Local Authorities that
+    overlap with the bounding box. Requires 'geometry' col in LA geopandas df.
+    Returns df of local authorities of interest.
+
+    Parameters
+    ----------
+    bbox: BoundingBox
+        Bounding box coordinates of the tweet
+
+    la_df: pd.DataFrame
+        (pandas) DataFrame containing information of
+        the Local Authorities (keys) to match
+
+    return_all: bool
+        Flag controlling whether to return only the top
+        matching local authorities or all of them
+        (ranked by likelihood). By default, False.
+
+    Returns
+    -------
+    A tuple containing the name, the code, and the reference of 
+    the top matching LA, or all of them (in the form of 
+    a pd.DataFrame)
+    """
+
+    if len(bbox) == 1:
+        # This is the case of using geo.coordinates
+        # for tweets
+        bbox = Point(bbox[0])
+    else:
+        polygon = Polygon(bbox)
+        if not polygon.is_valid:  # Try matching a Point
+            # Apply a slight correction to point coords
+            # to allow for better matching
+            bbox = (np.asarray(bbox) - 0.01)
+            bbox = Point(bbox[0])
+        else:
+            bbox = polygon
+
+    # Local Authorities of Interest are those that overlap with the bbox
+    laoi = la_df[la_df["geometry"].intersects(bbox)].copy()
+
+    if (laoi.shape[0] == 0): ## no overlap found
+        return None
+
+    # Intersection over the union is a measure of how exactly the bounding box and the la overlap
+    laoi["iou"] = la_df["geometry"].apply(
+        lambda g: g.intersection(bbox).area / g.union(bbox).area
+    )
+    # Pop weight is the proportion of the la population covered by the bounding box.
+    laoi["pop_weight"] = (
+        laoi["geometry"].apply(lambda g: (g.intersection(bbox).area / g.area)) * laoi["pop"]
+    )
+    # The final likelihood is the IoU multiplied by the population weight
+    laoi["likelihood"] = laoi["iou"] * laoi["pop_weight"]
+    # Sort dataframe by highest to lowest
+    laoi = laoi.sort_values(by="likelihood", ascending=False)
+
+    if return_all:
+        return laoi
+    return laoi["lad18nm"].iat[0], laoi["lad18cd"].iat[0], laoi["lhb"].iat[0]
+
+
+# %%
+def match_reference_la(data):
+    """ Choose LA with highest likelihood. Add LA and LHB to dataset. """
+
+    # Get a list of the required values from the first row of sorted dataframe.
+    def match_la(coords):
+        if coords is None:
+            return None
+        laoi = match_local_authorities(coords, la)
+        if laoi is None:
+            return None
+        return laoi
+
+    from datasets import load_local_authorities
+    la_ds = load_local_authorities()
+    la = la_ds.data
+
+    # Matching for Geo Coordinates - Those tweets having Geo-location info
+    set_geo_coords = set(data[~data['geo_coords'].isna()]['geo_coords'].values)
+    list_geo_coords = map(lambda c: [c], set_geo_coords)
+    la_map_geo_coords = {c: l for c, l in zip(set_geo_coords, map(match_la, list_geo_coords))}
+
+    data['la'] = data['geo_coords'].apply(lambda c: None if c is None or \
+                                          la_map_geo_coords[c] is None else la_map_geo_coords[c])
+
+    set_bbox_coords = set(data[data['la'].isna()]['bbox'].values)
+    la_map_bbox_coords = {c: l for c, l in zip(set_bbox_coords, map(match_la, set_bbox_coords))}
+    residual_matchings = data[data['la'].isna()]['bbox'].apply(lambda c: la_map_bbox_coords[c])
+    data['la'].fillna(residual_matchings, inplace=True)
+
+    # Split values for LA in three cols
+    data['lad18nm'] = data['la'].apply(lambda c: c[0])
+    data['lad18cd'] = data['la'].apply(lambda c: c[1])
+    data['lhb'] = data['la'].apply(lambda c: c[2])
+
+    # Drop la column - not needed anymore
+    data.drop('la', axis=1, inplace=True)
+
     return data
 
 
@@ -138,13 +284,15 @@ class TwitterPipeline(Pipeline):
         # define pipeline
         filter_welsh = partial(_get_welsh_tweets, col='place.full_name')
         combine_text = partial(_tidy_text_cols, col='extended_tweet.full_text')
-        geojson_bbox = partial(
-            _bbox_geojson, col='place.bounding_box.coordinates'),
+        geojson_bbox = partial(_bbox_geojson, col='place.bounding_box.coordinates')
+        convert_coords = partial(convert_coordinates, col='geo.coordinates')
 
         return [
             ('Filter Tweets from Wales', filter_welsh),
             ('Combine Text fields', combine_text),
-            ('Set DateTime Index', _create_datetime_index),
+            ('Convert Geo Coordinates (in Floating Point)', convert_coords),
             ('Collect BoundingBox Coordinates (GeoJSON)', geojson_bbox),
-            ('Collect BoundingBox Coordinates (Tuples)', _bbox_tuple)
+            ('Collect BoundingBox Coordinates (Tuples)', _bbox_tuple),
+            ('Match Local Authorities', match_reference_la),
+            ('Set DateTime Index', _create_datetime_index),
         ]
--- tweet-processing/tweet_geo_functions.py
+++ tweet-processing/tweet_geo_functions.py
@@ -3,63 +3,93 @@
 import pandas as pd
 import numpy as np
 from shapely.geometry import Polygon, Point
+from typing import Sequence, Tuple, Union
+
+Lat = float
+Long = float
+BoundingBox = Sequence[Tuple[Long, Lat]]
+MatchingLA = Union[Tuple[str, str, str], pd.DataFrame]
+# %%
+# def get_laoi(bbox: BoundingBox, la_df: pd.DataFrame, return_all: bool = False)->MatchingLA:
+#     """ Get the Intersection Over Union for the the Local Authorities that
+#     overlap with the bounding box. Requires 'geometry' col in LA geopandas df.
+#     Returns df of local authorities of interest.
+
+#     Parameters
+#     ----------
+#     bbox: BoundingBox
+#         Bounding box coordinates of the tweet
+
+#     la_df: pd.DataFrame
+#         (pandas) DataFrame containing information of
+#         the Local Authorities (keys) to match
+
+#     return_all: bool
+#         Flag controlling whether to return only the top
+#         matching local authorities or all of them
+#         (ranked by likelihood). By default, False.
+
+#     Returns
+#     -------
+#     A tuple containing the name, the code, and the reference of 
+#     the top matching LA, or all of them (in the form of 
+#     a pandas.DataFrame)
+#     """
+
+#     if len(bbox) == 1:
+#         # This is the case of using geo.coordinates
+#         # for tweets
+#         bbox = Point(bbox[0])
+#     else:
+#         polygon = Polygon(bbox)
+#         if not polygon.is_valid:  # Try matching a Point
+#             # Apply a slight correction to point coords
+#             # to allow for better matching
+#             bbox = (np.asarray(bbox) - 0.01)
+#             bbox = Point(bbox[0])
+#         else:
+#             bbox = polygon
+
+#     # Local Authorities of Interest are those that overlap with the bbox
+#     laoi = la_df[la_df["geometry"].intersects(bbox)].copy()
+
+#     if (laoi.shape[0] == 0): ## no overlap found
+#         return None
+
+#     # Intersection over the union is a measure of how exactly the bounding box and the la overlap
+#     laoi["iou"] = la_df["geometry"].apply(
+#         lambda g: g.intersection(bbox).area / g.union(bbox).area
+#     )
+#     # Pop weight is the proportion of the la population covered by the bounding box.
+#     laoi["pop_weight"] = (
+#         laoi["geometry"].apply(lambda g: (g.intersection(bbox).area / g.area))
+#         * laoi["pop"]
+#     )
+#     # The final likelihood is the IoU multiplied by the population weight
+#     laoi["likelihood"] = laoi["iou"] * laoi["pop_weight"]
+#     # Sort dataframe by highest to lowest
+#     laoi = laoi.sort_values(by="likelihood", ascending=False)
+
+#     if return_all:
+#         return laoi
+#     return laoi["lad18nm"].iat[0], laoi["lad18cd"].iat[0], laoi["lhb"].iat[0]
+
 
 # %%
-def get_laoi(bbox_tweet, la):
-    """ Get the Intersection Over Union for the the Local Authorities that
-    overlap with the bounding box. Requires 'geometry' col in LA geopandas df. 
-    Returns df of local authorities of interest. """
+# def add_reference_la(data, la):
+#     """ Choose LA with highest likelihood. Add LA and LHB to dataset. """
 
-    if not isinstance(bbox_tweet, Polygon):
-        polygon = Polygon(bbox_tweet)
-        if not polygon.is_valid:  # Try matching a Point
-            # Apply a slight correction to point coords
-            # to allow for better matching
-            bbox_tweet = (np.asarray(bbox_tweet) - 0.01)
-            bbox_tweet = Point(bbox_tweet[0])
-        else:
-            bbox_tweet = polygon
+#     # Get a list of the required values from the first row of sorted dataframe.
+#     def laoi_classes(bbox):
 
-    # Local Authorities of Interest are those that overlap with the bbox
-    laoi = la[la["geometry"].intersects(bbox_tweet)].copy()
+#         laoi = get_laoi(bbox, la)
+#         if laoi is None:
+#             return '', '', ''
 
-    if (laoi.shape[0] == 0): ## no overlap found
-        return None
+#         return laoi["lad18nm"].iat[0], laoi["lad18cd"].iat[0], laoi["lhb"].iat[0]
 
-    # Intersection over the union is a measure of how exactly the bounding box and the la overlap
-    laoi["iou"] = la["geometry"].apply(
-        lambda g: g.intersection(bbox_tweet).area / g.union(bbox_tweet).area
-    )
-
-    # Pop weight is the proportion of the la population covered by the bounding box.
-    laoi["pop_weight"] = (
-        laoi["geometry"].apply(lambda g: (g.intersection(bbox_tweet).area / g.area))
-        * laoi["pop"]
-    )
-
-    # The final likelihood is the IoU multiplied by the population weight
-    laoi["likelihood"] = laoi["iou"] * laoi["pop_weight"]
-
-    return laoi
-
-
-# %%
-def add_reference_la(data, la):
-    """ Choose LA with highest likelihood. Add LA and LHB to dataset. """
-
-    # Get a list of the required values from the first row of sorted dataframe.
-    def laoi_classes(bbox_tweet):
-
-        laoi = get_laoi(bbox_tweet, la)
-        if laoi is None:
-            return '', '', ''
-
-        # Sort dataframe by highest to lowest
-        laoi = laoi.sort_values(by="likelihood", ascending=False)
-        return laoi["lad18nm"].iat[0], laoi["lad18cd"].iat[0], laoi["lhb"].iat[0]
-
-    data[["lad18nm", "lad18cd", "lhb"]] = data["bbox_shapely"].apply(laoi_classes)
-    return data
+#     data[["lad18nm", "lad18cd", "lhb"]] = data["bbox_shapely"].apply(laoi_classes)
+#     return data
 
 
 # %%
