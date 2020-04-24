5b54bbad534f25ac18bbc3739208b889681d9ef0
Nina Di Cara <ninadicara@gmail.com>
2020-04-24 14:28:09 +0100

    Remove whitespace from welsh lang LSOA codes

--- data/raw_data_cleaning.py
+++ data/raw_data_cleaning.py
@@ -3,7 +3,7 @@ import pandas as pd
 import geopandas as gpd
 
 # %% Read in the LSOA reference data
-lsoa = gpd.read_file("../geography/boundaries_LSOAs.geojson")
+lsoa = gpd.read_file("geography/Lower_Layer_Super_Output_Areas_December_2011_Boundaries_EW_BGC.geojson")
 # %% Remove English data from lsoa
 lsoa = lsoa[lsoa['LSOA11CD'].str.contains("W", na=False)]
 
@@ -13,12 +13,20 @@ lsoa = lsoa[lsoa['LSOA11CD'].str.contains("W", na=False)]
 #++++++++
 
 # %% Read in the data
-welsh = pd.read_csv("lsoa_welsh_language_2011.csv",
+welsh = pd.read_csv("raw/lsoa_welsh_language_2011.csv",
                     usecols=[2, 3])
 
 # %% Fix Welsh col names and drop rows with no LSOA code
 welsh.rename(columns={'Unnamed: 2': 'LSOA11CD'}, inplace=True)
 welsh.dropna(subset=['LSOA11CD'], inplace=True)
+
+# %% Strip whitespace from the end of the LSOA11CD
+welsh['LSOA11CD'] = welsh['LSOA11CD'].apply(lambda x: x.strip())
+
+# %% Join the Welsh speaking data with the LSOA
+welsh_tidy = lsoa.merge(welsh, on='LSOA11CD', how="inner")
+
+# %% Write cleaned data to CSV with necessary columns
 
 
 #+++++++++++
