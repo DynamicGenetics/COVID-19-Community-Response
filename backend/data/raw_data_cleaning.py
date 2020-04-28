3a25c97d3abe458f7ecf145f547de9f65eab5dd5
Nina Di Cara <ninadicara@gmail.com>
2020-04-28 16:31:54 +0100

    Change variable names to constants in uppercase

--- backend/data/raw_data_cleaning.py
+++ backend/data/raw_data_cleaning.py
@@ -3,11 +3,11 @@ import pandas as pd
 import geopandas as gpd
 
 # %% Read in the LSOA reference data
-lsoa = gpd.read_file(
+LSOA = gpd.read_file(
     "static/geoboundaries/Lower_Layer_Super_Output_Areas_December_2011_Boundaries_EW_BSC.geojson"
 )
-# %% Remove English data from lsoa
-lsoa = lsoa[lsoa["LSOA11CD"].str.contains("W", na=False)]
+# %% Remove English data from LSOA
+LSOA = LSOA[LSOA["LSOA11CD"].str.contains("W", na=False)]
 
 
 # ++++++++
@@ -15,28 +15,28 @@ lsoa = lsoa[lsoa["LSOA11CD"].str.contains("W", na=False)]
 # ++++++++
 
 # %% Read in the data
-welsh = pd.read_csv("raw/lsoa_welsh_language_2011.csv", usecols=[2, 3])
+WELSH = pd.read_csv("raw/lsoa_welsh_language_2011.csv", usecols=[2, 3])
 
 # %% Fix Welsh col names and drop rows with no LSOA code
-welsh.rename(
+WELSH.rename(
     columns={
         "Unnamed: 2": "LSOA11CD",
         "Percentage able to speak Welsh ": "percent_welsh_speakers",
     },
     inplace=True,
 )
-welsh.dropna(subset=["LSOA11CD"], inplace=True)
+WELSH.dropna(subset=["LSOA11CD"], inplace=True)
 # lastly, reset the index after dropping rows.
-welsh.reset_index(drop=True, inplace=True)
+WELSH.reset_index(drop=True, inplace=True)
 
 # %% Strip whitespace from LSOA11CD (ready to merge)
-welsh["LSOA11CD"] = welsh["LSOA11CD"].apply(lambda x: x.strip())
+WELSH["LSOA11CD"] = WELSH["LSOA11CD"].apply(lambda x: x.strip())
 
 # %% Join the Welsh speaking data with the LSOA
-welsh_tidy = lsoa.merge(welsh, on="LSOA11CD", how="inner")
+WELSH_CLEAN = LSOA.merge(welsh, on="LSOA11CD", how="inner")
 
 # %% Write cleaned data to CSV with necessary columns
-welsh_tidy.to_csv(
+WELSH_CLEAN.to_csv(
     "cleaned/lsoa_welsh_speakers.csv",
     columns=["LSOA11CD", "LSOA11NM", "percent_welsh_speakers"],
     index=False,
@@ -46,14 +46,14 @@ welsh_tidy.to_csv(
 # POULATION
 # +++++++++++
 # %%
-population = pd.read_csv("raw/2019_pop.csv", usecols=[4, 7, 8], encoding="ISO-8859-1")
+POPULATION = pd.read_csv("raw/2019_pop.csv", usecols=[4, 7, 8], encoding="ISO-8859-1")
 
 # +++++++++++++
 # POP DENSITY
 # +++++++++++++
 # %%
 # Read from Excel file, sheet index 3
-popdensity = pd.read_excel(
+POPDENSITY = pd.read_excel(
     "raw/mid2018_popdensity_engandwales.xlsx", sheet_name=3, usecols="A,B,E", skiprows=4
 )
 
@@ -63,4 +63,4 @@ popdensity = pd.read_excel(
 # ++++++
 
 # %%
-imd = pd.read_csv("raw/2019IMD_deciles.csv", usecols=[1, 2], encoding="ISO-8859-1")
+IMD = pd.read_csv("raw/2019IMD_deciles.csv", usecols=[1, 2], encoding="ISO-8859-1")
