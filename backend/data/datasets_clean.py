a01eb92495aa150d12d4a55b544bca6af6f925dd
Nina Di Cara <ninadicara@gmail.com>
2020-05-03 17:15:53 +0100

    Include all raw datasets in pipeline

--- backend/data/datasets_clean.py
+++ backend/data/datasets_clean.py
@@ -2,149 +2,214 @@
 import pandas as pd
 import geopandas as gpd
 
-# Import functions from local module 
+# Import functions from local module
 from data_cleaning_functions import clean_data
 
 # Import the data constants from local module
 import datasets_raw as raw
 
-#++++++++++++++++++++++++++
+print("Raw datasets imported.")
+# ++++++++++++++++++++++++++
 # What does this script do?
-#+++++++++++++++++++++++++++
+# +++++++++++++++++++++++++++
 # The requirements for the cleaning functions are saved to a dict for each dataset
-# and the constant definitions calls these to be applied to the raw constants. 
+# and the constant definitions calls these to be applied to the raw constants.
 # Data is saved out to /cleaned
 
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Create dictionary of specifications for each variable
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 # example_dict = {
 #         "data": raw.DATANAME,
 #         "res": 'LSOA' OR 'LA',
 #         "key_col": , #name of col which is the key,
-#         "key_is_code": , #bool of whether the key is a code, if not it is a name 
+#         "key_is_code": , #bool of whether the key is a code, if not it is a name
 #         "keep_cols": [] for all, or a list of columns you specifically want to keep in the cleaned df.
 #         "csv_name": name of csv (resolution not needed)
-#optional "bracketed_data_cols": ,  #list of cols where there data in the format (DATA (PERCENT))
-#optional "rename_dict": , #dictionary of columns that need renaming
+# optional "bracketed_data_cols": ,  #list of cols where there data in the format (DATA (PERCENT))
+# optional "rename_dict": , #dictionary of columns that need renaming
 #          }
 
 lsoa_welsh_dict = {
-        "data": raw.WELSH_LSOA,
-        "res": 'LSOA',
-        "key_col": 'Unnamed: 2',
-        "key_is_code": True,
-        "rename_dict": {"Percentage able to speak Welsh ": "welsh_speakers_percent"},
-        "keep_cols": [],
-        "csv_name": 'welsh_speakers_percent'
-        }
-        
-lsoa_population_dict = {
-        "data": raw.POPULATION_LSOA,
-        "res": 'LSOA',
-        "key_col": "Area Codes",
-        "key_is_code": True,
-        "rename_dict": {'All Ages': 'population_count'},
-        "keep_cols": [],
-        "csv_name": 'population_count'
-        }
-
-lsoa_over_65_dict = {
-        "data": raw.OVER_65_LSOA,
-        "res": 'LSOA',
-        "key_col": "Area Codes",
-        "key_is_code": True,
-        "keep_cols": [],
-        "csv_name": 'over_65_count'
-        }
-
-lsoa_imd_dict = {
-        "data": raw.IMD_LSOA,
-        "res": 'LSOA',
-        "key_col": "lsoa11cd",
-        "key_is_code": True,
-        "keep_cols": ['LSOA11CD', 'wimd_2019'],
-        "csv_name": 'wimd_2019'
-        }
-
-lsoa_popdensity_dict = {
-        "data": raw.POPDENSITY_LSOA,
-        "res": 'LSOA',
-        "key_col": "Code",
-        "key_is_code": True,
-        "rename_dict": {'People per Sq Km': 'pop_density_persqkm'},
-        "keep_cols": ['LSOA11CD', 'pop_density_persqkm'],
-        "csv_name": 'pop_density_persqkm'
-        }
-
-la_vulnerable_dict = {
-        "data": raw.VULNERABLE_LA,
-        "res": "LA",
-        "key_col": "index",
-        "key_is_code": False,
-        "rename_dict": {1: 'n',
-                        38: 'vulnerable'},
-        "bracketed_data_cols": ['vulnerable'],
-        "keep_cols": [],
-        "csv_name": 'vulnerable_count_percent'
-        }
-
-la_cohesion_dict = {
-        "data": raw.COMM_COHESION_LA,
-        "res": "LA",
-        "key_col": "index",
-        "key_is_code": False,
-        "rename_dict": {1: 'n',
-                        20: 'belong_strongagree',
-                        21: 'belong_agree'},
-        "bracketed_data_cols": ['belong_strongagree', 'belong_agree'],
-        "keep_cols": [],
-        "csv_name": 'comm_cohesion_count_percent'
-        }
-
-la_internet_access_dict = {
-        "data": raw.INTERNET_ACCESS_LA,
-        "res": "LA",
-        "key_col": "Unnamed: 0",
-        "key_is_code": False,
-        "rename_dict": {'Yes (%)': 'has_internet_percent'},
-        "keep_cols": [],
-        "csv_name": 'has_internet_percent'
-        }
-
-la_internet_use_dict = {
-        "data": raw.INTERNET_USE_LA,
-        "res": "LA",
-        "key_col": "Unnamed: 0",
-        "key_is_code": False,
-        "rename_dict": {'Several times a day (%)': 'use_several_daily_percent',
-                        'Daily (%) ': 'use_daily_percent'},
-        "keep_cols": [],
-        "csv_name": 'use_internet_percent'
-        }
-
-la_ethnicity_dict = {
-        "data": ETHNICITY_LA,
-        "res": "LA",
-        "key_col": "Unnamed: 1",
-        "key_is_code": False,
-        "keep_cols": [],
-        "csv_name": 'ethnicities_percent'
+    "data": raw.WELSH_LSOA,
+    "res": "LSOA",
+    "key_col": "Unnamed: 2",
+    "key_is_code": True,
+    "rename_dict": {"Percentage able to speak Welsh ": "welsh_speakers_percent"},
+    "keep_cols": [],
+    "csv_name": "welsh_speakers_percent",
 }
 
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+la_welsh_dict = {
+    "data": raw.WELSH_LA,
+    "res": "LA",
+    "key_col": "Unnamed: 1",
+    "key_is_code": False,
+    "rename_dict": {
+        "Daily ": "welsh_daily_percent",
+        "Weekly ": "welsh_weekly_percent",
+        "Less Often ": "welsh_lessoften_percent",
+    },
+    "keep_cols": [],
+    "csv_name": "welsh_speakers_percent",
+}
+
+lsoa_population_dict = {
+    "data": raw.POPULATION_LSOA,
+    "res": "LSOA",
+    "key_col": "Area Codes",
+    "key_is_code": True,
+    "rename_dict": {"All Ages": "population_count"},
+    "keep_cols": [],
+    "csv_name": "population_count",
+}
+
+la_population_dict = {
+    "data": raw.POPULATION_LA,
+    "res": "LA",
+    "key_col": "Unnamed: 3",
+    "key_is_code": False,
+    "rename_dict": {"All ages .1": "population_count"},
+    "keep_cols": [],
+    "csv_name": "population_count",
+}
+
+lsoa_over_65_dict = {
+    "data": raw.OVER_65_LSOA,
+    "res": "LSOA",
+    "key_col": "Area Codes",
+    "key_is_code": True,
+    "keep_cols": [],
+    "csv_name": "over_65_count",
+}
+
+la_over_65_dict = {
+    "data": raw.OVER_65_LA,
+    "res": "LA",
+    "key_col": "Unnamed: 3",
+    "key_is_code": False,
+    "rename_dict": {"Unnamed: 14": "over_65_count"},
+    "keep_cols": [],
+    "csv_name": "over_65_count",
+}
+
+lsoa_imd_dict = {
+    "data": raw.IMD_LSOA,
+    "res": "LSOA",
+    "key_col": "lsoa11cd",
+    "key_is_code": True,
+    "keep_cols": ["LSOA11CD", "wimd_2019"],
+    "csv_name": "wimd_2019",
+}
+
+la_imd_dict = {
+    "data": raw.IMD_LA,
+    "res": "LA",
+    "key_col": "Unnamed: 0",
+    "key_is_code": False,
+    "keep_cols": [],
+    "csv_name": "wimd_2019",
+}
+
+lsoa_popdensity_dict = {
+    "data": raw.POPDENSITY_LSOA,
+    "res": "LSOA",
+    "key_col": "Code",
+    "key_is_code": True,
+    "rename_dict": {"People per Sq Km": "pop_density_persqkm"},
+    "keep_cols": ["LSOA11CD", "pop_density_persqkm"],
+    "csv_name": "pop_density_persqkm",
+}
+
+la_popdensity_dict = {
+    "data": raw.POPDENSITY_LA,
+    "res": "LA",
+    "key_col": "Unnamed: 1",
+    "key_is_code": False,
+    "rename_dict": {"Mid-year 2018 ": "pop_density_persqkm"},
+    "keep_cols": [],
+    "csv_name": "pop_density_persqkm",
+}
+
+la_vulnerable_dict = {
+    "data": raw.VULNERABLE_LA,
+    "res": "LA",
+    "key_col": "index",
+    "key_is_code": False,
+    "rename_dict": {38: "vulnerable"},
+    "bracketed_data_cols": ["vulnerable"],
+    "keep_cols": [],
+    "csv_name": "vulnerable_count_percent",
+}
+
+la_cohesion_dict = {
+    "data": raw.COMM_COHESION_LA,
+    "res": "LA",
+    "key_col": "index",
+    "key_is_code": False,
+    "rename_dict": {20: "belong_strongagree", 21: "belong_agree"},
+    "bracketed_data_cols": ["belong_strongagree", "belong_agree"],
+    "keep_cols": [],
+    "csv_name": "comm_cohesion_count_percent",
+}
+
+la_internet_access_dict = {
+    "data": raw.INTERNET_ACCESS_LA,
+    "res": "LA",
+    "key_col": "Unnamed: 0",
+    "key_is_code": False,
+    "rename_dict": {"Yes (%)": "has_internet_percent"},
+    "keep_cols": [],
+    "csv_name": "has_internet_percent",
+}
+
+la_internet_use_dict = {
+    "data": raw.INTERNET_USE_LA,
+    "res": "LA",
+    "key_col": "Unnamed: 0",
+    "key_is_code": False,
+    "rename_dict": {
+        "Several times a day (%)": "use_several_daily_percent",
+        "Daily (%) ": "use_daily_percent",
+    },
+    "keep_cols": [],
+    "csv_name": "use_internet_percent",
+}
+
+la_ethnicity_dict = {
+    "data": raw.ETHNICITY_LA,
+    "res": "LA",
+    "key_col": "Unnamed: 1",
+    "key_is_code": False,
+    "keep_cols": [],
+    "csv_name": "ethnicities_percent",
+}
+
+print("Dictionaries loaded.")
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 # Apply functions to each dataset, and create new constant
-#++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 WELSH_LSOA = clean_data(**lsoa_welsh_dict)
+
+WELSH_LA = clean_data(**la_welsh_dict)
 
 POPULATION_LSOA = clean_data(**lsoa_population_dict)
 
+POPULATION_LA = clean_data(**la_population_dict)
+
 OVER_65_LSOA = clean_data(**lsoa_over_65_dict)
+
+OVER_65_LA = clean_data(**la_over_65_dict)
 
 IMD_LSOA = clean_data(**lsoa_imd_dict)
 
+IMD_LA = clean_data(**la_imd_dict)
+
 POPDENSITY_LSOA = clean_data(**lsoa_popdensity_dict)
+
+POPDENSITY_LA = clean_data(**la_popdensity_dict)
 
 VULNERABLE_LA = clean_data(**la_vulnerable_dict)
 
--- backend/data/datasets_raw.py
+++ backend/data/datasets_raw.py
@@ -1,73 +1,87 @@
 import pandas as pd
-import geopandas as gpd
 
-#++++++++++++++++++++++++++
+# ++++++++++++++++++++++++++
 # What does this script do?
-#+++++++++++++++++++++++++++
+# +++++++++++++++++++++++++++
 # It reads in each file from the raw/folder in turn.
-# Some files in this folder contain mutiple data columns so these are seperated out 
-# for clarity to different constants. 
+# Some files in this folder contain mutiple data columns so these are seperated out
+# to different dataframe constants for clarity.
 
-# Read Welsh Data
+# It also adds some variables together to create summary varbs
+
+# ++++++++++++++++++++++++++++++++++
+# Read data into acceptable format
+# ++++++++++++++++++++++++++++++++++
 WELSH_LSOA = pd.read_csv("raw/lsoa_welsh_language_2011.csv", usecols=[2, 3])
 
+WELSH_LA = pd.read_csv("raw/la_welsh_frequency_2018-19.csv", usecols=[1, 2, 3, 4])
+
+
 # Read Population data (includes age based data)
-POPULATION_LSOA = pd.read_excel("raw/lsoa_population_2018-19.xlsx",
-                             sheet_name="Mid-2018 Persons", #Sheet 4
-                             usecols="A, D", #Reads columns - Area Codes, All Ages
-                             skiprows=4 # Data starts on row 5
-                             )
-
-OVER_65_LSOA = pd.read_excel("raw/lsoa_population_2018-19.xlsx",
-                             sheet_name="Mid-2018 Persons", #Sheet 4
-                             usecols="A, BR:CQ", #Reads columns - Area Codes, 65:90+
-                             skiprows=4 # Data starts on row 5
-                             )
-# Read in IMD data 
-IMD_LSOA = pd.read_csv("raw/lsoa_IMD_2019.csv")
-
-# Read in population density data 
-POPDENSITY_LSOA = pd.read_excel(
-    "raw/lsoa_pop_density_2018-19.xlsx",
-    sheet_name=3,
-    usecols="A,B,E",
-    skiprows=4
+POPULATION_LSOA = pd.read_excel(
+    "raw/lsoa_population_2018-19.xlsx",
+    sheet_name="Mid-2018 Persons",  # Sheet 4
+    usecols="A, D",  # Reads columns - Area Codes, All Ages
+    skiprows=4,  # Data starts on row 5
 )
 
+OVER_65_LSOA = pd.read_excel(
+    "raw/lsoa_population_2018-19.xlsx",
+    sheet_name="Mid-2018 Persons",  # Sheet 4
+    usecols="A, BR:CQ",  # Reads columns - Area Codes, 65:90+
+    skiprows=4,  # Data starts on row 5
+)
+
+POPULATION_LA = pd.read_csv("raw/la_population_age_2019.csv", usecols=[3, 15])
+OVER_65_LA = pd.read_csv("raw/la_population_age_2019.csv", usecols=[3, 14])
+
+
+# Read in IMD data
+IMD_LSOA = pd.read_csv("raw/lsoa_IMD_2019.csv")
+
+IMD_LA = pd.read_csv("raw/la_WIMD_2019.csv")
+
+# Read in population density data
+POPDENSITY_LSOA = pd.read_excel(
+    "raw/lsoa_pop_density_2018-19.xlsx", sheet_name=3, usecols="A,B,E", skiprows=4
+)
+
+POPDENSITY_LA = pd.read_csv("raw/la_pop_density_2018.csv", usecols=[1, 11])
+
 # Read in Vulnerable and Community Cohesion Data
-vulnerable_and_cohesion = pd.read_excel("raw/la_vulnerableProxy_and_cohesion.xlsx",
-                             sheet_name="By local authority", #Sheet 4
-                             )
+vulnerable_and_cohesion = pd.read_excel(
+    "raw/la_vulnerableProxy_and_cohesion.xlsx",
+    sheet_name="By local authority",
+    usecols="B:X",  # Sheet 4
+)
 # Select only the columns of interest and transpose
 vulnerable_and_cohesion = vulnerable_and_cohesion.iloc[[1, 20, 21, 38]].T
 # Reset index so the LA name isn't the index
 vulnerable_and_cohesion.reset_index(inplace=True)
-#Seperate this dataframe out so it only contains one variable per dataframe
+# Seperate this dataframe out so it only contains one variable per dataframe
+VULNERABLE_LA = vulnerable_and_cohesion.iloc[:, [0, 4]].copy()
+COMM_COHESION_LA = vulnerable_and_cohesion.iloc[:, [0, 2, 3]].copy()
 
-VULNERABLE_LA = vulnerable_and_cohesion.iloc[:, [0, 1, 4]].copy()
-
-COMM_COHESION_LA = vulnerable_and_cohesion.iloc[:, [0, 1, 2, 3]].copy()
 
 INTERNET_ACCESS_LA = pd.read_excel(
     "raw/National Survey results - internet use and freqency of access by local authority.xlsx",
     usecols="A,B",
-    skiprows=4, # Data starts on row 5
-    nrows=22 #Only parse 22 rows as there is more data underneath
+    skiprows=4,  # Data starts on row 5
+    nrows=22,  # Only parse 22 rows as there is more data underneath
 )
 
-# NB Here we aren't reading in the last column, because it is half empty. 
-# Instead we will recreate it in 'create_variables.py'
+# NB Here we aren't reading in the last column, because it is half empty.
 INTERNET_USE_LA = pd.read_excel(
     "raw/National Survey results - internet use and freqency of access by local authority.xlsx",
     usecols="A,B,C",
-    skiprows=34, # Data starts on row 5
-    nrows=22 #Only parse 22 necessary rows
+    skiprows=34,  # Data starts on row 5
+    nrows=22,  # Only parse 22 necessary rows
 )
 
+# This data is formatted the wrong way in the spreadsheet so needs extra work
 ETHNICITY_LA = pd.read_excel(
-    "raw/la_lhb_ethnicity.xlsx",
-    sheet_name="By Local Authority",
-    usecols="B:X").T
+    "raw/la_lhb_ethnicity.xlsx", sheet_name="By Local Authority", usecols="B:X"
+).T
 ETHNICITY_LA.reset_index(inplace=True)
 ETHNICITY_LA.rename(columns=ETHNICITY_LA.iloc[0], inplace=True)
 ETHNICITY_LA.drop(ETHNICITY_LA.index[0], inplace=True)
