19ad082ed85e3a7e38c2cb037cadf39854e44750
OliverDavis <oliver.davis@bristol.ac.uk>
2020-05-05 15:20:01 +0100

    added frontend v2 to repository

---
+++ frontend/js/sctr.js
@@ -0,0 +1,20 @@
+// Linked scatterplot module for covid communities
+// Copyright (C) Oliver Davis 2020
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version. See https://www.gnu.org/licenses/
+
+let sctr = (function(d3){
+
+  // Add the scatterplot
+  function draw(targetArea, x, y){
+    
+
+  }
+
+  return {
+    draw: draw
+  };
+})(d3);
