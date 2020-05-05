a7301a781bd2f476159e9cec4dc861e30fff4bb6
Nina Di Cara <ninadicara@gmail.com>
2020-05-05 23:26:48 +0100

    Updated variables names to avoid clash with _clean

--- backend/data/static/datasets_raw.py
+++ backend/data/static/datasets_raw.py
@@ -10,41 +10,41 @@ import pandas as pd
 # ++++++++++++++++++++++++++++++++++
 # Read data into acceptable format
 # ++++++++++++++++++++++++++++++++++
-WELSH_LSOA = pd.read_csv("raw/lsoa_welsh_language_2011.csv", usecols=[2, 3])
+RAW_WELSH_LSOA = pd.read_csv("raw/lsoa_welsh_language_2011.csv", usecols=[2, 3])
 
-WELSH_LA = pd.read_csv("raw/la_welsh_frequency_2018-19.csv", usecols=[1, 2, 3, 4])
+RAW_WELSH_LA = pd.read_csv("raw/la_welsh_frequency_2018-19.csv", usecols=[1, 2, 3, 4])
 
 
 # Read Population data (includes age based data)
-POPULATION_LSOA = pd.read_excel(
+RAW_POPULATION_LSOA = pd.read_excel(
     "raw/lsoa_population_2018-19.xlsx",
     sheet_name="Mid-2018 Persons",  # Sheet 4
     usecols="A, D",  # Reads columns - Area Codes, All Ages
     skiprows=4,  # Data starts on row 5
 )
 
-OVER_65_LSOA = pd.read_excel(
+RAW_OVER_65_LSOA = pd.read_excel(
     "raw/lsoa_population_2018-19.xlsx",
     sheet_name="Mid-2018 Persons",  # Sheet 4
     usecols="A, BR:CQ",  # Reads columns - Area Codes, 65:90+
     skiprows=4,  # Data starts on row 5
 )
 
-POPULATION_LA = pd.read_csv("raw/la_population_age_2019.csv", usecols=[3, 15])
-OVER_65_LA = pd.read_csv("raw/la_population_age_2019.csv", usecols=[3, 14])
+RAW_POPULATION_LA = pd.read_csv("raw/la_population_age_2019.csv", usecols=[3, 15])
+RAW_OVER_65_LA = pd.read_csv("raw/la_population_age_2019.csv", usecols=[3, 14])
 
 
 # Read in IMD data
-IMD_LSOA = pd.read_csv("raw/lsoa_IMD_2019.csv")
+RAW_IMD_LSOA = pd.read_csv("raw/lsoa_IMD_2019.csv")
 
-IMD_LA = pd.read_csv("raw/la_WIMD_2019.csv")
+RAW_IMD_LA = pd.read_csv("raw/la_WIMD_2019.csv")
 
 # Read in population density data
-POPDENSITY_LSOA = pd.read_excel(
+RAW_POPDENSITY_LSOA = pd.read_excel(
     "raw/lsoa_pop_density_2018-19.xlsx", sheet_name=3, usecols="A,B,E", skiprows=4
 )
 
-POPDENSITY_LA = pd.read_csv("raw/la_pop_density_2018.csv", usecols=[1, 11])
+RAW_POPDENSITY_LA = pd.read_csv("raw/la_pop_density_2018.csv", usecols=[1, 11])
 
 # Read in Vulnerable and Community Cohesion Data
 vulnerable_and_cohesion = pd.read_excel(
@@ -57,11 +57,11 @@ vulnerable_and_cohesion = vulnerable_and_cohesion.iloc[[1, 20, 21, 38]].T
 # Reset index so the LA name isn't the index
 vulnerable_and_cohesion.reset_index(inplace=True)
 # Seperate this dataframe out so it only contains one variable per dataframe
-VULNERABLE_LA = vulnerable_and_cohesion.iloc[:, [0, 4]].copy()
-COMM_COHESION_LA = vulnerable_and_cohesion.iloc[:, [0, 2, 3]].copy()
+RAW_VULNERABLE_LA = vulnerable_and_cohesion.iloc[:, [0, 4]].copy()
+RAW_COMM_COHESION_LA = vulnerable_and_cohesion.iloc[:, [0, 2, 3]].copy()
 
 
-INTERNET_ACCESS_LA = pd.read_excel(
+RAW_INTERNET_ACCESS_LA = pd.read_excel(
     "raw/National Survey results - internet use and freqency of access by local authority.xlsx",
     usecols="A,B",
     skiprows=4,  # Data starts on row 5
@@ -69,7 +69,7 @@ INTERNET_ACCESS_LA = pd.read_excel(
 )
 
 # NB Here we aren't reading in the last column, because it is half empty.
-INTERNET_USE_LA = pd.read_excel(
+RAW_INTERNET_USE_LA = pd.read_excel(
     "raw/National Survey results - internet use and freqency of access by local authority.xlsx",
     usecols="A,B,C",
     skiprows=34,  # Data starts on row 5
@@ -77,10 +77,10 @@ INTERNET_USE_LA = pd.read_excel(
 )
 
 # This data is formatted the wrong way in the spreadsheet so needs extra work
-ETHNICITY_LA = pd.read_excel(
+RAW_ETHNICITY_LA = pd.read_excel(
     "raw/la_lhb_ethnicity.xlsx", sheet_name="By Local Authority", usecols="B:X"
 ).T
-ETHNICITY_LA.reset_index(inplace=True)
-ETHNICITY_LA.rename(columns=ETHNICITY_LA.iloc[0], inplace=True)
-ETHNICITY_LA.drop(ETHNICITY_LA.index[0], inplace=True)
-ETHNICITY_LA.drop(ETHNICITY_LA.columns[1], axis=1, inplace=True)
+RAW_ETHNICITY_LA.reset_index(inplace=True)
+RAW_ETHNICITY_LA.rename(columns=RAW_ETHNICITY_LA.iloc[0], inplace=True)
+RAW_ETHNICITY_LA.drop(RAW_ETHNICITY_LA.index[0], inplace=True)
+RAW_ETHNICITY_LA.drop(RAW_ETHNICITY_LA.columns[1], axis=1, inplace=True)
