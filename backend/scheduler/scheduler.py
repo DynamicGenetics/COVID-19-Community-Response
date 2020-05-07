c21f27825b9266dd5ba2e14cf5f7c43fe1bf1f10
Al Tanner <8190834+altanner@users.noreply.github.com>
2020-05-07 16:21:37 +0100

    plumbing
    needs plumbing to mongo_ops, mongo_ops needs fields as arguments.

--- backend/scheduler/scheduler.py
+++ backend/scheduler/scheduler.py
@@ -2,7 +2,7 @@ import schedule
 
 # Some schedule scripts for dynamic genetics groups projects
 
-def schedule(run_frequency_in_days):
+def schedule(run_frequency_in_days, the_thing_you_want_to_schedule):
 
     """ Schedule sets up a time to execute other tasks.
     When waiting to run, your python script will be in an Sl state:
@@ -18,15 +18,16 @@ def schedule(run_frequency_in_days):
     schedule.every().minute.at(":17").do(job)"""
 
     # run task at 6amUTC (night for most of Europe and US: less rate limiting)
-    schedule.every(run_frequency_in_days).days.at("06:00").do(main)
+    schedule.every(run_frequency_in_days).days.at("06:00").do(the_thing_you_want_to_schedule)
 
 
 def main_scheduler(run_frequency_in_days):
 
     if __name__ == '__main__':
+        
         main() # run the process immediately on calling the script.
-        schedule(run_frequency_in_days) # repeat time
-
+        schedule.every(run_frequency_in_days).days.at("06:00").do(main)
+        
         while True:
             schedule.run_pending()
             time.sleep(42) # wait a few seconds in case anything needs to finish up
@@ -34,7 +35,7 @@ def main_scheduler(run_frequency_in_days):
 
 def mongoexport_scheduler(run_frequency_in_days):
 
-    schedule(run_frequency_in_days) # repeat time
+    schedule.every(run_frequency_in_days).days.at("06:00").do(mongo_ops.export_csv(mongoexport_executable_path, csv_filename))
 
     while True:
         schedule.run_pending()
