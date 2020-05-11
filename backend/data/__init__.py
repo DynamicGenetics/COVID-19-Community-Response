88d83ae1b9007252a1419f8075ebf7fe99e957d6
leriomaggio <valerio.maggio@gmail.com>
2020-05-11 13:20:43 +0100

    moved paths constants to main package file

---
+++ backend/data/__init__.py
@@ -0,0 +1,5 @@
+import os
+
+BASE_FOLDER = os.path.abspath(os.path.dirname('__file__'))
+SOURCE_DATA_FOLDER = os.path.join(BASE_FOLDER, 'static', 'source')
+GEO_DATA_FOLDER = os.path.join(BASE_FOLDER, 'static', 'geoboundaries')
