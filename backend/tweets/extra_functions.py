e774a950d4adf225d55ca1c8701f269e8eddd735
Nina Di Cara <ninadicara@gmail.com>
2020-05-22 12:44:37 +0100

    Consolidate additional functions to one file

--- backend/tweets/extra_functions.py
+++ backend/tweets/extra_functions.py
@@ -1,9 +1,30 @@
-#####################################
-# USEFUL TWEET PROCESSING FUNCTIONS #
-#####################################
-
 from geojson import Feature, MultiPolygon, FeatureCollection
 import json
+
+# %%
+# In progress - how to define how certain the match is
+def class_uncertainty(laoi):
+    """ Roughly calculate how certain the classification is based on distances between
+    the likelihood of the potentially overlapping LA boundaries. """
+
+    # Sort dataframe by highest to lowest
+    laoi = laoi.sort_values(by="likelihood", ascending=False)
+    # Get the list of values
+    a = laoi["likelihood"]
+    a.reset_index().drop()
+    del l["index"]
+
+    # If there was only one, the
+    if len(a) == 1:
+        return 1
+    elif len(a) == 2:
+        return "only two"  # decide penalty function
+    else:
+        # Get the difference between each sequential number
+        b = a.diff(periods=-1)
+        # Divide the distance between the first and second number by the
+        b["likelihood"][0] / b[1:].mean()
+
 
 ##############################
 # Textual Analysis Functions #
@@ -30,25 +51,7 @@ def analyse_sentiment(data, col="text"):
     return data
 
 
-################################
-# Geodata Processing Functions #
-################################
-
-
-def split_coords(data, col="geo.coordinates"):
-    """ Takes a Pandas dataframe with a column geo.coordinates (col) and adds the lat and long to their own columns
-    for easy conversion to geojson. """
-
-    # Split the string on the comma
-    data["long"], data["lat"] = data[col].str.split(",", 1).str
-
-    # Remove the left over square brackets
-    data["lat"] = data["lat"].str[:-1]
-    data["long"] = data["long"].str[1:]
-
-    return data
-
-
+# No longer needed with geopandas
 def write_bbox_geojson(data, col="bbox_geojson"):
     """ Given a correctly formatted column of bbox polygons, this will convert them to
     a geojson object, and write it out to a file. """
