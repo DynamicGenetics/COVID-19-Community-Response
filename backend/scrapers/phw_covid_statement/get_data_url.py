0e2557ecfdeb7053e1f776358c761787c4345b94
Nina Di Cara <ninadicara@gmail.com>
2020-05-25 22:36:29 +0100

    Add quit() browser and try except section

--- backend/scrapers/phw_covid_statement/get_data_url.py
+++ backend/scrapers/phw_covid_statement/get_data_url.py
@@ -1,19 +1,33 @@
 from selenium import webdriver  # Import module
+from selenium.common.exceptions import NoSuchElementException
 import time  # Waiting function
+from warnings import warn
 
 
 def get_data_link():
-
+    """Use Selenium to open a Safari window and get the url link"""
     home_page_url = "https://public.tableau.com/views/RapidCOVID-19virology-Public/Headlinesummary?:display_count=y&:embed=y&:showAppBanner=false&:showVizHome=no"  # Define URL
-    xpath = '//*[@id="tabZoneId66"]/div/div/div/a'
 
-    browser = webdriver.Safari()
-    browser.get(home_page_url)
-    time.sleep(2)
-    # browser.maximize_window()
+    browser = webdriver.Safari()  # Open Safari
+    browser.get(home_page_url)  # Go to the homepage url
+    time.sleep(5)  # Wait for it to load
 
-    element = browser.find_element_by_id("tabZoneId66")
-    result = element.find_element_by_xpath(xpath)
-    url = result.get_attribute("href")
+    def get_url():
+        element = browser.find_element_by_id("tabZoneId66")
+        xpath = '//*[@id="tabZoneId66"]/div/div/div/a'
+        result = element.find_element_by_xpath(xpath)
+        return result.get_attribute("href")
+
+    try:
+        url = get_url()
+    except NoSuchElementException:
+        warn(
+            "Can't find the element. I'm going to wait 10 seconds \n"
+            "in case the webpage still needs to load."
+        )
+        time.sleep(10)
+        url = get_url()
+    finally:
+        browser.quit()
 
     return url
