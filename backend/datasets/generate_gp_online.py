ce48cc18cec328d448defd0d8176f2e398541006
Nina Di Cara <ninadicara@gmail.com>
2020-05-26 14:51:21 +0100

    Update file paths to use SOURCE_DATA_FOLDER

--- backend/datasets/generate_gp_online.py
+++ backend/datasets/generate_gp_online.py
@@ -2,24 +2,26 @@ import pandas as pd
 import numpy as np
 import os
 
+from datasets import SOURCE_DATA_FOLDER, LIVE_DATA_FOLDER
+
 # ------------------
 # Read in the data
 # ------------------
 
 GP_DATA = pd.read_excel(
-    "static/source/local/Digital Exclusion sources.xlsx",
+    os.path.join(SOURCE_DATA_FOLDER, "local", "Digital Exclusion sources.xlsx"),
     sheet_name="Pts Registered with MHOL",
     use_cols=["A", "C", "E:G"],
     skiprows=1,
     na_filter=False,
 )
 POSTCODES = pd.read_csv(
-    "static/source/local/PCD_OA_LSOA_MSOA_LAD_FEB19_UK_LU.csv",
+    os.path.join(SOURCE_DATA_FOLDER, "local", "PCD_OA_LSOA_MSOA_LAD_FEB19_UK_LU.csv"),
     encoding="ISO-8859-1",
     usecols=[2, 7, 8, 9, 10, 11, 12],  # Only include relevant cols
 )
 GP_LOOKUP = pd.read_csv(
-    "static/source/local/epraccur.csv",
+    os.path.join(SOURCE_DATA_FOLDER, "local", "epraccur.csv"),
     usecols=[0, 9],
     names=["practice_ID", "postcode"],
 )
@@ -67,7 +69,7 @@ def gp_to_area(gp_data, postcode_lookup, gp_lookup):
 
 def mhol_to_pct(df, LA: bool = False, LSOA: bool = False, MSOA: bool = False):
     """For a dataframe with rows of each GP practice mapped to LA or LSOA codes and names,
-    sums "patients_total" and "MHOL_true" across area, and creates new col with total percenatage 
+    sums "patients_total" and "MHOL_true" across area, and creates new col with total percentage
     over each LA.
 
     Arguments:
@@ -118,6 +120,6 @@ LSOA_COUNTS = mhol_to_pct(GP_AREAS, LSOA=True)
 MSOA_COUNTS = mhol_to_pct(GP_AREAS, MSOA=True)
 
 
-LA_COUNTS.to_csv(os.path.join("static", "source", "la_gp_online.csv"))
-LSOA_COUNTS.to_csv(os.path.join("static", "source", "lsoa_gp_online.csv"))
-MSOA_COUNTS.to_csv(os.path.join("static", "source", "msoa_gp_online.csv"))
+LA_COUNTS.to_csv(os.path.join(SOURCE_DATA_FOLDER, "la_gp_online.csv"))
+LSOA_COUNTS.to_csv(os.path.join(SOURCE_DATA_FOLDER, "lsoa_gp_online.csv"))
+MSOA_COUNTS.to_csv(os.path.join(SOURCE_DATA_FOLDER, "msoa_gp_online.csv"))
