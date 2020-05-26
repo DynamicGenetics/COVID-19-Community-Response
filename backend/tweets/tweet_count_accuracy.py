063fcbbf43ce65aee4c42fece94f206fc7dd5854
Nina Di Cara <ninadicara@gmail.com>
2020-05-26 14:41:28 +0100

    Generate current annotated data percentages

--- backend/tweets/tweet_count_accuracy.py
+++ backend/tweets/tweet_count_accuracy.py
@@ -6,7 +6,7 @@ from pipelines import TwitterPipeline
 from datasets import load_tweets, load_annotated_tweets
 import re
 from datasets import load_local_authorities
-
+from build_classifier import normalise_tweets
 
 # %% Load Tweets
 tweets = load_tweets()
@@ -19,44 +19,79 @@ annotated = load_annotated_tweets()
 # %%
 tweets = pd.merge(tweets, annotated, on="id_str", how="left")
 
+tweets["text_norm"] = normalise_tweets(tweets["text"])
+
 # %%
 # Can keywords produce useful subsets of the data?
-tw2 = tweets[tweets["text"].str.contains(r"help", regex=True, na=False)].copy()
+iso = tweets[
+    tweets["text_norm"].str.contains(
+        r"(\w{4})?\s?-?isolat | (\w{6})?\s?-?dist", regex=True, na=False
+    )
+].copy()
+tw2 = tweets[
+    tweets["text_norm"].str.contains(
+        r"help | support | need any ?thing", regex=True, na=False
+    )
+].copy()
+
 tw3 = tweets[
-    tweets["text"].str.contains(r"shop | food | medic | pharmac", regex=True, na=False)
+    tweets["text_norm"].str.contains(
+        r"shop | food | medic | pharmac", regex=True, na=False
+    )
 ].copy()
 tw4 = tweets[
-    tweets["text"].str.contains(
+    tweets["text_norm"].str.contains(
         r"street | neighbour | road | village | community | next ?door",
         regex=True,
         na=False,
     )
 ].copy()
+
+tw2_3 = tw2.merge(tw3["id_str"], how="inner", on="id_str", suffixes=("", "_y"))
+help_shop_comm = tw4.merge(
+    tw2_3["id_str"], how="inner", on="id_str", suffixes=("", "_y")
+)
+
 tw5 = tweets[
-    tweets["text"].str.contains(
+    tweets["text_norm"].str.contains(
         r"facebook | whatsapp | next ?door ", regex=True, na=False
     )
 ].copy()
-
+tw6 = tweets[tweets["text_norm"].str.contains("volunt", regex=True, na=False)]
 # Get combinations of the above to use
 dfs = {
+    "iso": iso,
+    "iso_help": iso.merge(tw2["id_str"], how="inner", on="id_str", suffixes=("", "_y")),
+    "iso_shop": iso.merge(tw3["id_str"], how="inner", on="id_str", suffixes=("", "_y")),
+    "iso_community": iso.merge(
+        tw4["id_str"], how="inner", on="id_str", suffixes=("", "_y")
+    ),
+    "iso_sm": iso.merge(tw5["id_str"], how="inner", on="id_str", suffixes=("", "_y")),
+    "iso_vol": iso.merge(tw6["id_str"], how="inner", on="id_str", suffixes=("", "_y")),
     "tw1": tweets[
-        tweets["text"].str.contains(
+        tweets["text_norm"].str.contains(
             r"community support | support group | community group", regex=True, na=False
         )
     ],
-    "tw2_3": tw2.merge(tw3["id_str"], how="inner", on="id_str", suffixes=("", "_y")),
+    "help_vol": tw2.merge(tw6["id_str"], how="inner", on="id_str", suffixes=("", "_y")),
+    "shop_vol": tw3.merge(tw6["id_str"], how="inner", on="id_str", suffixes=("", "_y")),
+    "tw2": tw2,
+    "tw3": tw3,
+    "tw4": tw4,
+    "tw5": tw5,
+    "tw6": tw6,
+    "tw2_3": tw2_3,
     "tw2_4": tw2.merge(tw4["id_str"], how="inner", on="id_str", suffixes=("", "_y")),
     "tw2_5": tw2.merge(tw5["id_str"], how="inner", on="id_str", suffixes=("", "_y")),
     "tw3_4": tw3.merge(tw4["id_str"], how="inner", on="id_str", suffixes=("", "_y")),
     "tw3_5": tw3.merge(tw5["id_str"], how="inner", on="id_str", suffixes=("", "_y")),
     "tw4_5": tw4.merge(tw5["id_str"], how="inner", on="id_str", suffixes=("", "_y")),
-    "tw6": tweets[tweets["text"].str.contains("volunt", regex=True, na=False)],
+    "help_shop_comm": help_shop_comm,
 }
 
 # Add tweets about shopping that aren't about online shopping
 online_tweets = tweets[
-    tweets["text"].str.contains(r"on ?line | click", regex=True, na=False)
+    tweets["text_norm"].str.contains(r"on ?line | click", regex=True, na=False)
 ]
 df = pd.merge(
     dfs["tw2_3"], online_tweets["id_str"], on="id_str", how="outer", indicator=True
@@ -65,6 +100,18 @@ dfs["tw7"] = df[df["_merge"] == "left_only"]
 
 # Join all of the tweets and add to dictionary
 dfs["tws"] = pd.concat(dfs.values()).drop_duplicates("id_str")  # Full subset
+
+new_rules = [
+    dfs["tw1"],
+    dfs["tw5"],
+    dfs["iso_shop"],
+    dfs["iso_vol"],
+    dfs["help_vol"],
+    dfs["shop_vol"],
+]
+dfs["new_rules"] = pd.concat(new_rules).drop_duplicates("id_str")
+
+dfs["new_rules"].to_csv("new_rules_tweets.csv")
 
 
 # %%
@@ -93,9 +140,8 @@ def calculate_accuracies(ground_truth_col: str, df_dict: dict = dfs):
 
 
 accuracies_ND = calculate_accuracies("support_ND")
-accuracies_LH = calculate_accuracies("support_LH")
+# accuracies_LH = calculate_accuracies("support_LH")
 
 print(accuracies_ND)
-print(accuracies_LH)
-
-# Outcome is fairly poor - need for more accurate classification
+# print(accuracies_LH)
+import pandas as pd
--- backend/tweets/tweet_map_counts.py
+++ backend/tweets/tweet_map_counts.py
@@ -6,6 +6,7 @@ from pipelines import TwitterPipeline
 from datasets import load_tweets
 from tweet_functions import analyse_sentiment
 import re
+import numpy as np
 
 
 # %% Load Tweets
@@ -13,76 +14,30 @@ tweets = load_tweets()
 # %% Filter the tweets from Wales and format the text
 tweets = TwitterPipeline().apply(tweets.data, verbosity=2)
 
-###############
-# EXPLORATION #
-###############
 
-# %%
-# How many tweets are defined by Twitter as being in Welsh?
-# len(tweets[tweets["lang"].str.contains("cy")])
-
-# %%
-# Can keywords produce useful subsets of the data?
-tw2 = tweets[tweets["text"].str.contains("help", regex=True, na=False)].copy()
-tw3 = tweets[
-    tweets["text"].str.contains("shop | food | medic | pharmac", regex=True, na=False)
-].copy()
-tw4 = tweets[
-    tweets["text"].str.contains(
-        "street | neighbour | road | village", regex=True, na=False
-    )
-].copy()
-tw5 = tweets[
-    tweets["text"].str.contains(
-        "facebook | whatsapp | next ?door ", regex=True, na=False
-    )
-].copy()
-
-# Get combinations of the above to use
-tw1 = tweets[
-    tweets["text"].str.contains(
-        "community support | support group | community group", regex=True, na=False
-    )
-]
-tw2_3 = tw2.merge(tw3["id_str"], how="inner", on="id_str", suffixes=("", "_y"))
-tw2_4 = tw2.merge(tw4["id_str"], how="inner", on="id_str", suffixes=("", "_y"))
-tw2_5 = tw2.merge(tw5["id_str"], how="inner", on="id_str", suffixes=("", "_y"))
-tw3_4 = tw3.merge(tw4["id_str"], how="inner", on="id_str", suffixes=("", "_y"))
-tw3_5 = tw3.merge(tw5["id_str"], how="inner", on="id_str", suffixes=("", "_y"))
-tw4_5 = tw4.merge(tw5["id_str"], how="inner", on="id_str", suffixes=("", "_y"))
-tw6 = tweets[tweets["text"].str.contains("volunt", regex=True, na=False)]
+tweets = pd.read_csv("new_rules_idstr_intact.csv")
 
 
-# %%
-# Join all of the tweets
-df_list = [tw1, tw2_3, tw2_4, tw2_5, tw3_4, tw3_5, tw4_5, tw6]  # 7093 tweets data
-tws = pd.concat(df_list).drop_duplicates("id_str")  # Full subset
+annotated = load_annotated_tweets()
+new_anno = tweets[["id_str", "support_ND"]]
+all_annos = pd.merge(annotated, new_anno, on="id_str", how="outer")
+all_annos["support_ND_x"].fillna(all_annos["support_ND_y"], inplace=True)
+all_annos["support_ND_x"] = all_annos["support_ND_x"].astype(str)
+all_annos.drop(columns=["support_ND_y"], inplace=True)
+all_annos.rename(columns={"support_ND_x": "support_ND"}, inplace=True)
+all_annos.to_pickle("new_annotations.pkl")
 
-# %%
-tws.shape
+tweets = pd.merge(all_annos, tweets, on="id_str", how="left")
+tweets_yes = tweets[tweets["support_ND"] == "1"]
+tweets_yes = tweets_yes[["user.id_str", "lad19cd"]]
 
-# %%
-# NEXT STEP - inspect - do the tweets look ok?
 
-# %%
-# Now, we want to group by local authority to prepare the dataset for mapping
-tws_out = tws.groupby(["lad19cd"]).count().reset_index()
+unique = tweets_yes.drop_duplicates(subset=["user.id_str", "lad19cd"])
 
-# %%
-from datasets import load_local_authorities
-
-la = load_local_authorities()
-la = la.data
-
-tws_out["count"] = tws_out["id_str"].copy()
-tws_out = pd.merge(
-    la, tws_out[["count", "lad18cd"]], left_on="lad18cd", right_on="lad18cd"
+table = pd.pivot_table(
+    unique, values="user.id_str", index="lad19cd", aggfunc=np.count_nonzero
 )
 
-# %%
-tws_out["tweets_per_pop"] = tws_out["count"] / tws_out["pop"]
-tws_out["tweets_per_pop"] /= tws_out["tweets_per_pop"].max()
 
-# %%
-tws_out = tws_out[["lad18cd", "lad18nm", "geometry", "tweets_per_pop"]]
-tws_out.to_file("../data/twitter_count.geojson", driver="GeoJSON")
+tweets = tweets[["user.id_str", "lad19cd"]]
+unique_all = tweets.drop_duplicates(subset=["user.id_str", "lad19cd"])
