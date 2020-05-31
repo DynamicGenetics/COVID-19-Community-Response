c0a1fe3b103a5d2397e1cec9c37bd4e8b34ff870
Nina Di Cara <ninadicara@gmail.com>
2020-05-31 23:20:17 +0100

    Update map counts as a function

--- backend/tweets/tweet_map_counts.py
+++ backend/tweets/tweet_map_counts.py
@@ -1,88 +1,45 @@
-# %%
-# Import Functions
-from datasets import load_local_authorities
+"""Function for generating user_counts data"""
 import pandas as pd
-import geopandas as gpd
+import numpy as np
+import os
 from pipelines import TwitterPipeline
-from datasets import load_tweets
-from tweet_functions import analyse_sentiment
-import re
+from datasets import load_tweets, load_annotated_tweets
 
 
-# %% Load Tweets
-tweets = load_tweets()
-# %% Filter the tweets from Wales and format the text
-tweets = TwitterPipeline().apply(tweets.data, verbosity=2)
+# Create the full dataset of annotated tweets and get +ve ones
+def generate_map_counts():
+    """Writes out percentage of positive user tweets to file
+    """
+    # Now load the full tweet dataset and find unique users per LA
+    tweets = load_tweets()
+    tweets = TwitterPipeline().apply(tweets.data, verbosity=2)
+    annotations = load_annotated_tweets()
 
-###############
-# EXPLORATION #
-###############
+    tweets_annotated = pd.merge(annotations, tweets, on="id_str", how="left")
+    tweets_yes = tweets_annotated[tweets_annotated["support_ND"] == "1"]
+    tweets_yes = tweets_yes[["user.id_str", "lad19cd"]]
 
-# %%
-# How many tweets are defined by Twitter as being in Welsh?
-# len(tweets[tweets["lang"].str.contains("cy")])
-
-# %%
-# Can keywords produce useful subsets of the data?
-tw2 = tweets[tweets["text"].str.contains("help", regex=True, na=False)].copy()
-tw3 = tweets[
-    tweets["text"].str.contains("shop | food | medic | pharmac", regex=True, na=False)
-].copy()
-tw4 = tweets[
-    tweets["text"].str.contains(
-        "street | neighbour | road | village", regex=True, na=False
+    # Get only unique users and create a table of counts by LA
+    unique = tweets_yes.drop_duplicates(subset=["user.id_str", "lad19cd"])
+    table = pd.pivot_table(
+        unique, values="user.id_str", index="lad19cd", aggfunc=np.count_nonzero
     )
-].copy()
-tw5 = tweets[
-    tweets["text"].str.contains(
-        "facebook | whatsapp | next ?door ", regex=True, na=False
+
+    tweets = tweets[["user.id_str", "lad19cd"]]
+    unique_all = tweets.drop_duplicates(subset=["user.id_str", "lad19cd"])
+    table_all = pd.pivot_table(
+        unique_all, values="user.id_str", index="lad19cd", aggfunc=np.count_nonzero
     )
-].copy()
 
-# Get combinations of the above to use
-tw1 = tweets[
-    tweets["text"].str.contains(
-        "community support | support group | community group", regex=True, na=False
+    # Now merge the two counts and create the overall percentage
+    user_counts = pd.merge(table, table_all, on="lad19cd")
+    user_counts["tweets_percentage"] = (
+        user_counts["user.id_str_x"] / user_counts["user.id_str_y"]
+    ) * 100
+    user_counts = user_counts[["tweets_percentage"]]
+    user_counts.to_csv(
+        os.path.join(
+            "..", "datasets", "data", "live", "cleaned", "community_tweets.csv"
+        )
     )
-]
-tw2_3 = tw2.merge(tw3["id_str"], how="inner", on="id_str", suffixes=("", "_y"))
-tw2_4 = tw2.merge(tw4["id_str"], how="inner", on="id_str", suffixes=("", "_y"))
-tw2_5 = tw2.merge(tw5["id_str"], how="inner", on="id_str", suffixes=("", "_y"))
-tw3_4 = tw3.merge(tw4["id_str"], how="inner", on="id_str", suffixes=("", "_y"))
-tw3_5 = tw3.merge(tw5["id_str"], how="inner", on="id_str", suffixes=("", "_y"))
-tw4_5 = tw4.merge(tw5["id_str"], how="inner", on="id_str", suffixes=("", "_y"))
-tw6 = tweets[tweets["text"].str.contains("volunt", regex=True, na=False)]
-
-
-# %%
-# Join all of the tweets
-df_list = [tw1, tw2_3, tw2_4, tw2_5, tw3_4, tw3_5, tw4_5, tw6]  # 7093 tweets data
-tws = pd.concat(df_list).drop_duplicates("id_str")  # Full subset
-
-# %%
-tws.shape
-
-# %%
-# NEXT STEP - inspect - do the tweets look ok?
-
-# %%
-# Now, we want to group by local authority to prepare the dataset for mapping
-tws_out = tws.groupby(["lad18cd"]).count().reset_index()
-
-# %%
-
-la = load_local_authorities()
-la = la.data
-
-tws_out["count"] = tws_out["id_str"].copy()
-tws_out = pd.merge(
-    la, tws_out[["count", "lad18cd"]], left_on="lad18cd", right_on="lad18cd"
-)
-
-# %%
-tws_out["tweets_per_pop"] = tws_out["count"] / tws_out["pop"]
-tws_out["tweets_per_pop"] /= tws_out["tweets_per_pop"].max()
-
-# %%
-tws_out = tws_out[["lad18cd", "lad18nm", "geometry", "tweets_per_pop"]]
-tws_out.to_file("../data/twitter_count.geojson", driver="GeoJSON")
+    return user_counts
