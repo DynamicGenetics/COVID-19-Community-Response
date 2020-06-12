b10799315005c2225a4a8b5d95542681c79897c4
Nina Di Cara <ninadicara@gmail.com>
2020-06-12 10:25:21 +0100

    Update senelium to headerless chrome

--- backend/scrapers/phw_covid_statement/get_data_url.py
+++ backend/scrapers/phw_covid_statement/get_data_url.py
@@ -1,14 +1,22 @@
 from selenium import webdriver  # Import module
+from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import NoSuchElementException
+from webdriver_manager.chrome import ChromeDriverManager
 import time  # Waiting function
 from warnings import warn
+import os
 
 
 def get_data_link():
     """Use Selenium to open a Safari window and get the url link of most recent dataset"""
     home_page_url = "https://public.tableau.com/views/RapidCOVID-19virology-Public/Headlinesummary?:display_count=y&:embed=y&:showAppBanner=false&:showVizHome=no"  # Define URL
 
-    browser = webdriver.Safari()  # Open Safari
+    # Set chrome to be headless (i.e. doesn't open the webpage GUI)
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+
+    # Set browser to be headerless Chrome
+    browser = webdriver.Chrome(ChromeDriverManager().install(), options=chrome_options)
     browser.get(home_page_url)  # Go to the homepage url
     time.sleep(5)  # Wait for it to load
 
