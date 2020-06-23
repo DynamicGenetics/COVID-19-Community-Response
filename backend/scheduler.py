3427bc53b5aaf1245409e7aa7c2430f44b8de66d
Nina Di Cara <ninadicara@gmail.com>
2020-06-23 16:59:42 +0100

    Updated data

--- backend/scheduler.py
+++ backend/scheduler.py
@@ -3,25 +3,40 @@ import time
 import functools
 import logging
 
+# NB Necessary to set up the logging config before importing the variables from other scripts
+logging.basicConfig(
+    filename="scheduler.log",
+    filemode="a",
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO,
+)
+
+# --------------
+# Local imports
+# --------------
 from generate_json import DATA
 
 from datasets import LIVE_DATA_FOLDER, LIVE_RAW_DATA_FOLDER, GEO_DATA_FOLDER
 from scrapers.police_coders_groups.run_scraper import run_police_coders_scraper
 from scrapers.phw_covid_statement.phw_scraper import run_phw_scraper
 
-# --------------
-# Set up logging
-# --------------
+# ---------------------
+# Logging wrap function
+# ---------------------
 
+# Set up logging object, logger
 logger = logging.getLogger(__name__)
 
 
+# Note: logging wrapper taken from docs https://schedule.readthedocs.io/en/stable/faq.html
 def with_logging(func):
+    """This is a wrapper that logs the start and end of a function."""
+
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
-        print('LOG: Running job "%s"' % func.__name__)
+        logger.info('Running job "%s"' % func.__name__)
         result = func(*args, **kwargs)
-        print('LOG: Job "%s" completed' % func.__name__)
+        logger.info('Job "%s" completed' % func.__name__)
         return result
 
     return wrapper
@@ -41,14 +56,14 @@ def run_scrapers():
 
 
 @with_logging
-def rewrite_json():
+def update_json():
     DATA.write()
 
 
 if __name__ == "__main__":
 
     run_scrapers()
-    DATA.write()
+    update_json()
 
     # # Run scrapers at 3pm UTC, which is 4pm UTC
     # schedule.every().day.at("15:00").do(job)
